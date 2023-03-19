d005829a776332f456ed480fa99cef6e44bf5c17
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-03-19 14:39:07 -0700

    Initial commit

---
+++ src/main/java/me/purplewolfmc/genesismc/GenesisMC.java
@@ -0,0 +1,17 @@
+package me.purplewolfmc.genesismc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GenesisMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
