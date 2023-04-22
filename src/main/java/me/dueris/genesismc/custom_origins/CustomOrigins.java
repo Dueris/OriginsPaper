0d597a84cad643cbe7611dd905493c7fb1264685
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-04-22 10:12:20 -0700

    removed some stuff for custom origins to start dev for normal origin datapacks

--- src/main/java/me/dueris/genesismc/custom_origins/CustomOrigins.java
+++ src/main/java/me/dueris/genesismc/custom_origins/CustomOrigins.java
@@ -29,61 +29,7 @@ public class CustomOrigins implements Listener {
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, filter)) {
                 for (Path path : stream) {
                     //begin checks
-                    if(path.toFile().exists()){
-                        if(path.toFile().list().length >= 2){
-                            if (GenesisDataFiles.getPlugCon().getString("console-dump-onstartup").equalsIgnoreCase("true")) {
-                                getServer().getConsoleSender().sendMessage("there are origins (" + path.toFile().list().length + ")");
-                            }else{
-                                //no message response
-                            }
-                            String maindir = path.toFile().getAbsolutePath();
-                            if (GenesisDataFiles.getPlugCon().getString("console-dump-onstartup").equalsIgnoreCase("true")) {
-                                getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[GenesisMC] Loading custom origins in dir of " + path.toFile().getAbsolutePath());
-                            }
-                            File originpack_datamain = new File(String.valueOf(path), "data");
-                            String datapath = originpack_datamain.getAbsolutePath();
-                            File originpack_powermain = new File(String.valueOf(path), "powers");
-                            String powerspath = originpack_powermain.getAbsolutePath();
-                            File originpack_mainclass = new File(String.valueOf(datapath), "main.yml");
-                            if(originpack_datamain.exists()){
-                                //check 1
-                                if (GenesisDataFiles.getPlugCon().getString("console-dump-onstartup").equalsIgnoreCase("true")) {
-                                    getServer().getConsoleSender().sendMessage("data folder loaded. check 1 complete");
-                                }else{
-                                    //do nothing
-                                }
-                                if(originpack_powermain.exists()){
-                                    //check 2
-                                    if (GenesisDataFiles.getPlugCon().getString("console-dump-onstartup").equalsIgnoreCase("true")) {
-                                        getServer().getConsoleSender().sendMessage("powers folder loaded. check 2 complete");
-                                    }
-                                    if(originpack_mainclass.exists()){
-                                        //check 3
-                                        if (GenesisDataFiles.getPlugCon().getString("console-dump-onstartup").equalsIgnoreCase("true")) {
-                                            getServer().getConsoleSender().sendMessage("main class loaded. check 3 complete");
-                                        }
-                                        YamlConfiguration mainconfig = new YamlConfiguration();
-                                        mainconfig = YamlConfiguration.loadConfiguration(originpack_mainclass);
-                                        if(mainconfig.getString("is-custom-origin").equalsIgnoreCase("true")){
-                                            getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[GenesisMC] Successfully loaded " + originpack_mainclass.getParentFile().getParentFile().getName());
 
-                                        }else{
-                                            getServer().getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] Origin pack is invalid");
-                                            getServer().getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] Disabling origin pack");
-                                            //do nothing
-                                        }
-
-                                        }else{
-                                        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] Error loading custom origin: CODE-001");
-                                    }
-                                }else{
-                                    getServer().getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] Error loading custom origin: CODE-002");
-                                }
-                            }else{
-                                getServer().getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] Error loading custom origin: CODE-003");
-                            }
-                        }
-                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
