f9dd831e7a717d82f8c56301a85e02d041e42225
Mapty231 <tye.exe@gmail.com>
2023-04-25 16:04:12 +0100

    Fixed beds not exploding in nether and end Fixed error with some allowed foods throwing null error in console

--- src/main/java/me/dueris/genesismc/core/origins/avian/AvianMain.java
+++ src/main/java/me/dueris/genesismc/core/origins/avian/AvianMain.java
@@ -4,6 +4,7 @@ import me.dueris.genesismc.core.GenesisMC;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -49,8 +50,8 @@ public class AvianMain implements Listener {
         PersistentDataContainer data = e.getPlayer().getPersistentDataContainer();
         int originid = data.get(new NamespacedKey(GenesisMC.getPlugin(), "originid"), PersistentDataType.INTEGER);
         if (originid == 4501011) {
-            @NotNull ItemStack item = e.getItem();
-
+            ItemStack item = e.getItem();
+            if (item == null) return;
             for (Material food : inedibleFoodAvian) {
                 if (item.getType() == food) {
                     e.setCancelled(true);
@@ -64,8 +65,10 @@ public class AvianMain implements Listener {
         PersistentDataContainer data = e.getPlayer().getPersistentDataContainer();
         int originid = data.get(new NamespacedKey(GenesisMC.getPlugin(), "originid"), PersistentDataType.INTEGER);
         if (originid == 4501011) {
-            if (e.getBed().getY() <= 99) {
-                e.setCancelled(true);
+            if (e.getPlayer().getWorld().getEnvironment() == World.Environment.NORMAL) {
+                if (e.getBed().getY() <= 99) {
+                    e.setCancelled(true);
+                }
             }
         }
     }
@@ -76,16 +79,18 @@ public class AvianMain implements Listener {
         int originid = data.get(new NamespacedKey(GenesisMC.getPlugin(), "originid"), PersistentDataType.INTEGER);
         if (originid == 4501011) {
             Player p = e.getPlayer();
-            Block block = e.getClickedBlock();
-            for (Material bed : beds) {
-                if(e.getClickedBlock() != null){  //added null check in dev
-                if (block.getType() == bed) {
-                    if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                        if (block.getY() <= 99) {
-                            e.setCancelled(true);
+            if (p.getWorld().getEnvironment() == World.Environment.NORMAL) {
+                Block block = e.getClickedBlock();
+                for (Material bed : beds) {
+                    if (e.getClickedBlock() != null) {  //added null check in dev
+                        if (block.getType() == bed) {
+                            if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                                if (block.getY() <= 99) {
+                                    e.setCancelled(true);
+                                }
+                            }
                         }
                     }
-                }
                 }
             }
         }
