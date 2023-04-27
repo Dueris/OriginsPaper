644af73f7d92ae22e7c72b71cb263c00123015ae
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-04-26 19:59:05 -0700

    added new choosing method files

---
+++ src/main/java/me/dueris/genesismc/core/choosing/ChoosingMenuCORE.java
@@ -0,0 +1,6 @@
+package me.dueris.genesismc.core.choosing;
+
+import org.bukkit.event.Listener;
+
+public class ChoosingMenuCORE implements Listener {
+}
---
+++ src/main/java/me/dueris/genesismc/core/choosing/ChoosingMenuEXP.java
@@ -0,0 +1,6 @@
+package me.dueris.genesismc.core.choosing;
+
+import org.bukkit.event.Listener;
+
+public class ChoosingMenuEXP implements Listener {
+}
