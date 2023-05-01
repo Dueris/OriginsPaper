4a57e15cc7c84ddb4b37cf40ac15d048e3df020f
Mapty231 <tye.exe@gmail.com>
2023-05-01 03:49:19 +0100

    Partial conversion to origintags, not sure what tags should be what

--- src/main/java/me/dueris/genesismc/core/utils/ParticleHandler.java
+++ src/main/java/me/dueris/genesismc/core/utils/ParticleHandler.java
@@ -16,9 +16,11 @@ public class ParticleHandler extends BukkitRunnable {
             PersistentDataContainer data = p.getPersistentDataContainer();
             @Nullable String origintag = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
             if (p.getGameMode() != GameMode.SPECTATOR && !p.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
+                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (originid == 0401065) {
                     p.getWorld().spawnParticle(Particle.PORTAL, p.getLocation(), 4, 0.6F, -0.5, 0.6F);
                 }
+                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (originid == 7303065) {
                     p.getWorld().spawnParticle(Particle.GLOW, p.getLocation(), 1, 0.35F, -0.5, 0.35F);
                     Particle.DustTransition dustColorTransition = new Particle.DustTransition(Color.fromRGB(42, 191, 232), Color.fromRGB(191, 0, 255), 1F);
@@ -33,6 +35,7 @@ public class ParticleHandler extends BukkitRunnable {
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getEyeLocation(), 2, 0.35F, -0.5, 0.35F, whitedust);
 
                 }
+                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (originid == 1205048) {
                     Particle.DustTransition dustColorTransition = new Particle.DustTransition(Color.fromRGB(97, 255, 250), Color.fromRGB(163, 255, 244), 0.275F);
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getLocation(), 3, 0.4F, -0.5, 0.4F, dustColorTransition);
