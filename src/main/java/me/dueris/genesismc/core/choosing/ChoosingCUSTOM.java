cbc45196a66d65b5a69a6356b12ab21000a7d00a
Mapty231 <tye.exe@gmail.com>
2023-05-01 23:22:14 +0100

    Fixed custom origins menu displaying un choose-able origins

--- src/main/java/me/dueris/genesismc/core/choosing/ChoosingCUSTOM.java
+++ src/main/java/me/dueris/genesismc/core/choosing/ChoosingCUSTOM.java
@@ -18,13 +18,10 @@ import java.util.ArrayList;
 
 public class ChoosingCUSTOM implements Listener {
 
-    static ArrayList<ItemStack> contents = new ArrayList<>();
-    static ArrayList<String> customOriginTags = CustomOriginsMethods.getCustomOriginTags();
-
     @EventHandler(priority = EventPriority.HIGH)
-    public void CUSOTMCHOOSE_MENU(InventoryClickEvent e){
-        if(e.getCurrentItem() != null){
-            if(e.getView().getTitle().equalsIgnoreCase("Choosing Menu")) {
+    public void CUSOTMCHOOSE_MENU(InventoryClickEvent e) {
+        if (e.getCurrentItem() != null) {
+            if (e.getView().getTitle().equalsIgnoreCase("Choosing Menu")) {
                 @NotNull Inventory custommenu = Bukkit.createInventory(e.getWhoClicked(), 54, "Custom Origins");
                 if (e.getCurrentItem().getType().equals(Material.TIPPED_ARROW)) {
                     Player p = (Player) e.getWhoClicked();
@@ -62,20 +59,45 @@ public class ChoosingCUSTOM implements Listener {
                     nextmeta.setLore(nextlore);
                     next.setItemMeta(nextmeta);
 
+                    ArrayList<ItemStack> contents = new ArrayList<>();
+                    ArrayList<String> customOriginTags = CustomOriginsMethods.getCustomOriginTags();
 
                     for (int i = 0; i <= 53; i++) {
-                        if (i%9 == 0 || (i+1)%9 == 0) {
+                        if (i % 9 == 0 || (i + 1) % 9 == 0) {
                             contents.add(new ItemStack(Material.IRON_BARS));
-                        } else if (i==46) {
+                        } else if (i == 46) {
                             contents.add(back);
-                        } else if (i==49) {
+                        } else if (i == 49) {
                             contents.add(menu);
-                        } else if (i==52) {
+                        } else if (i == 52) {
                             contents.add(next);
                         } else if (i >= 46) {
                             contents.add(new ItemStack(Material.AIR));
                         } else {
-                            displayNextOrigin();
+                            if (customOriginTags.size() > 0) {
+                                String origintag = customOriginTags.get(0);
+                                while (CustomOriginsMethods.getCustomOriginUnChoosable(origintag)) {
+                                    customOriginTags.remove(0);
+                                    origintag = customOriginTags.get(0);
+                                }
+                                String minecraftItem = CustomOriginsMethods.getCustomOriginIcon(origintag);
+                                String item = minecraftItem.split(":")[1];
+                                ItemStack originIcon = new ItemStack(Material.valueOf(item.toUpperCase()));
+
+                                ItemMeta meta = originIcon.getItemMeta();
+                                meta.setDisplayName(CustomOriginsMethods.getCustomOriginName(origintag));
+                                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                                ArrayList<String> lore = new ArrayList<>();
+                                lore.add(CustomOriginsMethods.getCustomOriginDescription(origintag));
+                                meta.setLore(lore);
+                                originIcon.setItemMeta(meta);
+
+                                contents.add(originIcon);
+
+                                customOriginTags.remove(0);
+                            } else {
+                                contents.add(new ItemStack(Material.AIR));
+                            }
                         }
                     }
 
@@ -84,39 +106,6 @@ public class ChoosingCUSTOM implements Listener {
 
                 }
             }
-        }
-    }
-
-    public static void displayNextOrigin() {
-        if (customOriginTags.size() > 0) {
-            String origintag = customOriginTags.get(0);
-            System.out.println(CustomOriginsMethods.getCustomOriginUnChoosable(origintag));
-            if (CustomOriginsMethods.getCustomOriginUnChoosable(origintag)) {
-                customOriginTags.remove(0);
-                try {
-                    displayNextOrigin();
-                } catch (Exception e) {
-                    ;
-                }
-            } else {
-                String minecraftItem = CustomOriginsMethods.getCustomOriginIcon(origintag);
-                String item = minecraftItem.split(":")[1];
-                ItemStack originIcon = new ItemStack(Material.valueOf(item.toUpperCase()));
-
-                ItemMeta meta = originIcon.getItemMeta();
-                meta.setDisplayName(CustomOriginsMethods.getCustomOriginName(origintag));
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                ArrayList<String> lore = new ArrayList<>();
-                lore.add(CustomOriginsMethods.getCustomOriginDescription(origintag));
-                meta.setLore(lore);
-                originIcon.setItemMeta(meta);
-
-                contents.add(originIcon);
-
-                customOriginTags.remove(0);
-            }
-        } else {
-            contents.add(new ItemStack(Material.AIR));
         }
     }
 }
