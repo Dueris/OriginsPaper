562dbb98fdcd0aec128e6d4206955c469852debc
Mapty231 <tye.exe@gmail.com>
2023-05-03 01:24:14 +0100

    Added haste effect on all not tools for shulk. Fixed nat blocks dropping two itms for Shulk

--- src/main/java/me/dueris/genesismc/core/origins/shulk/ShulkMain.java
+++ src/main/java/me/dueris/genesismc/core/origins/shulk/ShulkMain.java
@@ -17,6 +17,20 @@ import java.util.*;
 import static org.bukkit.Material.*;
 public class ShulkMain implements Listener {
 
+  public static EnumSet<Material> tools;
+  public static EnumSet<Material> nat_stones;
+
+  static {
+    nat_stones = EnumSet.of(GRANITE, COBBLED_DEEPSLATE, TUFF, STONE, ANDESITE, BLACKSTONE, COBBLESTONE, CALCITE, AMETHYST_BLOCK, ANDESITE_SLAB,
+            ANDESITE_STAIRS, ANDESITE_WALL, DIORITE, DIORITE_SLAB, DIORITE_STAIRS, DIORITE_WALL, BLACKSTONE_SLAB, BLACKSTONE_STAIRS, BLACKSTONE_WALL,
+            COAL_ORE, DEEPSLATE, DEEPSLATE_COAL_ORE, NETHERRACK, END_STONE);
+      tools = EnumSet.of(
+              WOODEN_PICKAXE, STONE_PICKAXE, GOLDEN_PICKAXE, IRON_PICKAXE, DIAMOND_PICKAXE, NETHERITE_PICKAXE,
+              WOODEN_AXE, STONE_AXE, GOLDEN_AXE, IRON_AXE, DIAMOND_AXE, NETHERITE_AXE,
+              WOODEN_SHOVEL, STONE_SHOVEL, GOLDEN_SHOVEL, IRON_SHOVEL, DIAMOND_SHOVEL, NETHERITE_SHOVEL,
+              SHEARS);
+  }
+
   @EventHandler
   public void onSprint(PlayerMoveEvent e) {
     Player p = e.getPlayer();
@@ -36,8 +50,6 @@ public class ShulkMain implements Listener {
     }
   }
 
-  public static EnumSet<Material> nat_stones;
-  public static EnumSet<Material> tool;
 
   @EventHandler
   public void onBreakShulk(BlockBreakEvent e) {
@@ -47,17 +59,19 @@ public class ShulkMain implements Listener {
     @Nullable String origintag = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
     if (origintag.equalsIgnoreCase("genesis:origin-shulk")) {
       if (nat_stones.contains(e.getBlock().getType())) {
-        if (!p.getGameMode().equals(GameMode.CREATIVE)) {
-          if (e.getBlock().getType().equals(STONE)) {
-            p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COBBLESTONE));
-          } else if (e.getBlock().getType().toString().contains("coal") && e.getBlock().getType().toString().contains("ore")) {
-            p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COAL));
-          } else if (e.getBlock().getType().equals(DEEPSLATE)) {
-            p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COBBLED_DEEPSLATE));
-          } else {
-            p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), i);
+        if (!tools.contains(p.getEquipment().getItemInMainHand().getType())) {
+          if (!p.getGameMode().equals(GameMode.CREATIVE)) {
+            if (e.getBlock().getType().equals(STONE)) {
+              p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COBBLESTONE));
+            } else if (e.getBlock().getType().toString().contains("coal") && e.getBlock().getType().toString().contains("ore")) {
+              p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COAL));
+            } else if (e.getBlock().getType().equals(DEEPSLATE)) {
+              p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(COBBLED_DEEPSLATE));
+            } else {
+              p.getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), i);
+            }
           }
-      }
+        }
       }
     }
   }
@@ -154,10 +168,5 @@ public class ShulkMain implements Listener {
       }
     }
   }
-
-    static {
-      nat_stones = EnumSet.of(GRANITE, COBBLED_DEEPSLATE, TUFF, STONE, ANDESITE, BLACKSTONE, COBBLESTONE, CALCITE, AMETHYST_BLOCK, ANDESITE_SLAB, ANDESITE_STAIRS, ANDESITE_WALL, DIORITE, DIORITE_SLAB, DIORITE_STAIRS, DIORITE_WALL, BLACKSTONE_SLAB, BLACKSTONE_STAIRS, BLACKSTONE_WALL, COAL_ORE, DEEPSLATE, DEEPSLATE_COAL_ORE, NETHERRACK, END_STONE);
-      tool = EnumSet.of(Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SHOVEL, Material.DIAMOND_SWORD, Material.GOLDEN_AXE, Material.GOLDEN_HOE, Material.GOLDEN_PICKAXE, Material.GOLDEN_SHOVEL, Material.GOLDEN_SWORD, Material.NETHERITE_AXE, Material.NETHERITE_HOE, Material.NETHERITE_PICKAXE, Material.NETHERITE_SHOVEL, Material.NETHERITE_SWORD, Material.IRON_AXE, Material.IRON_HOE, Material.IRON_PICKAXE, Material.IRON_SHOVEL, Material.IRON_SWORD, Material.WOODEN_AXE, Material.WOODEN_HOE, Material.WOODEN_PICKAXE, Material.WOODEN_SHOVEL, Material.WOODEN_SWORD, Material.SHEARS);
-    }
-  }
+}
 
