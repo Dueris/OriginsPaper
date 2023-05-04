2f3bfa50d3c3cb539f92c664a674e2fdfc0d4320
Mapty231 <tye.exe@gmail.com>
2023-05-05 00:39:29 +0100

    put nether spawn as its own power

--- src/main/java/me/dueris/genesismc/core/origins/piglin/PiglinMain.java
+++ src/main/java/me/dueris/genesismc/core/origins/piglin/PiglinMain.java
@@ -3,6 +3,7 @@ package me.dueris.genesismc.core.origins.piglin;
 import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import me.dueris.genesismc.core.GenesisMC;
+import me.dueris.genesismc.custom_origins.powers.AlternateSpawns;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
@@ -134,37 +135,9 @@ public class PiglinMain implements Listener {
         @Nullable String origintag = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
         if (origintag.equalsIgnoreCase("genesis:origin-piglin")) {
             if (!(e.isBedSpawn() || e.isAnchorSpawn())) {
-                for (World world : Bukkit.getWorlds()) {
-                    if (world.getEnvironment() == World.Environment.NETHER) {
-
-                        Random random = new Random();
-                        Location location = new Location(world, random.nextInt(-300, 300), 32, random.nextInt(-300, 300));
-
-                        respawnLocationSearch:
-                        for (int x = (int) (location.getX()-100); x < location.getX()+100; x++) {
-                            for (int z = (int) (location.getZ()-100); z < location.getZ()+100; z++) {
-                                yLoop:
-                                for (int y = (int) (location.getY()); y < location.getY()+68; y++) {
-                                    if (new Location(world, x, y, z).getBlock().getType() != AIR) continue;
-                                    if (new Location(world, x, y+1, z).getBlock().getType() != AIR) continue;
-                                    Material blockBeneath = new Location(world, x, y-1, z).getBlock().getType();
-                                    if (blockBeneath == AIR || blockBeneath == LAVA || blockBeneath == FIRE || blockBeneath == SOUL_FIRE) continue;
-
-                                    for (int potentialX = (int) (new Location(world, x, y, z).getX()-2); potentialX < new Location(world, x, y, z).getX()+2; potentialX++) {
-                                        for (int potentialY = (int) (new Location(world, x, y, z).getY()); potentialY < new Location(world, x, y, z).getY()+2; potentialY++) {
-                                            for (int potentialZ = (int) (new Location(world, x, y, z).getZ()-2); potentialZ < new Location(world, x, y, z).getZ()+2; potentialZ++) {
-                                                if (new Location(world, potentialX, potentialY, potentialZ).getBlock().getType() != AIR) continue yLoop;
-                                            }
-                                        }
-                                    }
-                                    e.setRespawnLocation(new Location(world, x+0.5, y, z+0.5));
-                                    break respawnLocationSearch;
-                                }
-                            }
-                        }
-                        break;
-                    }
-                }
+                Location location = AlternateSpawns.NetherSpawn();
+                if (location == null) return;
+                e.setRespawnLocation(location);
             }
 
         }
---
+++ src/main/java/me/dueris/genesismc/custom_origins/powers/AlternateSpawns.java
@@ -0,0 +1,46 @@
+package me.dueris.genesismc.custom_origins.powers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+import java.util.Random;
+
+import static org.bukkit.Material.*;
+
+public class AlternateSpawns {
+
+    public static Location NetherSpawn() {
+        for (World world : Bukkit.getWorlds()) {
+            if (world.getEnvironment() == World.Environment.NETHER) {
+
+                Random random = new Random();
+                Location location = new Location(world, random.nextInt(-300, 300), 32, random.nextInt(-300, 300));
+
+                for (int x = (int) (location.getX()-100); x < location.getX()+100; x++) {
+                    for (int z = (int) (location.getZ()-100); z < location.getZ()+100; z++) {
+                        yLoop:
+                        for (int y = (int) (location.getY()); y < location.getY()+68; y++) {
+                            if (new Location(world, x, y, z).getBlock().getType() != AIR) continue;
+                            if (new Location(world, x, y+1, z).getBlock().getType() != AIR) continue;
+                            Material blockBeneath = new Location(world, x, y-1, z).getBlock().getType();
+                            if (blockBeneath == AIR || blockBeneath == LAVA || blockBeneath == FIRE || blockBeneath == SOUL_FIRE) continue;
+
+                            for (int potentialX = (int) (new Location(world, x, y, z).getX()-2); potentialX < new Location(world, x, y, z).getX()+2; potentialX++) {
+                                for (int potentialY = (int) (new Location(world, x, y, z).getY()); potentialY < new Location(world, x, y, z).getY()+2; potentialY++) {
+                                    for (int potentialZ = (int) (new Location(world, x, y, z).getZ()-2); potentialZ < new Location(world, x, y, z).getZ()+2; potentialZ++) {
+                                        if (new Location(world, potentialX, potentialY, potentialZ).getBlock().getType() != AIR) continue yLoop;
+                                    }
+                                }
+                            }
+                            return (new Location(world, x+0.5, y, z+0.5));
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        return null;
+    }
+}
