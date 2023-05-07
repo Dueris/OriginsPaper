457721ecf37de8c56b0c4f175668e239b382a2b1
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-06 19:35:22 -0700

    added orb of origin particles

--- src/main/java/me/dueris/genesismc/core/items/Items.java
+++ src/main/java/me/dueris/genesismc/core/items/Items.java
@@ -1,25 +1,57 @@
 package me.dueris.genesismc.core.items;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
 import java.util.ArrayList;
 
+import static me.dueris.genesismc.core.items.OrbOfOrigins.orb;
 import static org.bukkit.ChatColor.GRAY;
 import static org.bukkit.Material.ENDER_PEARL;
 
 
-public class Items implements Listener {
+public class Items extends BukkitRunnable implements Listener {
 
+    public static Location getRightSide(Location location, double distance) {
+        float angle = location.getYaw() / 60;
+        return location.clone().subtract(new Vector(Math.cos(angle), 0, Math.sin(angle)).normalize().multiply(distance));
+    }
+
+    public static Location getLeftSide(Location location, double distance) {
+        float angle = location.getYaw() / 60;
+        return location.clone().add(new Vector(Math.cos(angle), 0, Math.sin(angle)).normalize().multiply(distance));
+    }
+
+    Particle.DustTransition whitedust = new Particle.DustTransition(Color.fromRGB(139, 243, 252), Color.fromRGB(224, 253, 255), 0.7F);
+
+    @Override
+    public void run() {
+        for(Player p : Bukkit.getOnlinePlayers()){
+
+            if(p.getInventory().getItemInMainHand().equals(orb)){
+                Location rightHand = getRightSide(p.getEyeLocation(), 0.45).subtract(0, .6, 0);
+                p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, rightHand, 1, 0.25F, -0.5, 0.25F, whitedust);
+            }
+            if(p.getInventory().getItemInOffHand().equals(orb)){
+                Location leftHand = getLeftSide(p.getEyeLocation(), 0.45).subtract(0, .6, 0);
+                p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, leftHand, 1, 0.25F, -0.5, 0.25F, whitedust);
+            }
+        }
+    }
 
     @EventHandler
-    public void oncraftattempt(PrepareItemCraftEvent e){
+    public void OnCraftAttempt(PrepareItemCraftEvent e){
         ItemStack infinpearl = new ItemStack(ENDER_PEARL);
 
         ItemMeta pearl_meta = infinpearl.getItemMeta();
@@ -31,32 +63,19 @@ public class Items implements Listener {
         infinpearl.setItemMeta(pearl_meta);
         
         ItemStack spectatorswitch = new ItemStack(Material.PHANTOM_MEMBRANE);
-
         ItemMeta switch_meta = spectatorswitch.getItemMeta();
-
         switch_meta.setDisplayName(GRAY + "Phantom Form");
-
         pearl_lore = new ArrayList();
-
         switch_meta.setUnbreakable(true);
-
         switch_meta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
-
         switch_meta.addItemFlags(ItemFlag.HIDE_ITEM_SPECIFICS);
-
         switch_meta.setLore(pearl_lore);
-
         switch_meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-
         spectatorswitch.setItemMeta(switch_meta);
+
         if(e.getInventory().contains(infinpearl) || e.getInventory().contains(spectatorswitch)){
             e.getInventory().setResult(null);
         }
     }
-
-
-      
-
-
 
 }
