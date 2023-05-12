751c8335e4822897db4f61d86e670e05ec8f308e
Mapty231 <tye.exe@gmail.com>
2023-05-12 21:08:26 +0100

    Registered HotHands event, and SlowFalling runnable

--- src/main/java/me/dueris/genesismc/core/factory/powers/OriginStartHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginStartHandler.java
@@ -1,18 +1,16 @@
 package me.dueris.genesismc.core.factory.powers;
 
 import me.dueris.genesismc.core.GenesisMC;
-import me.dueris.genesismc.core.bukkitrunnables.EnderianDamageRunnable;
 import me.dueris.genesismc.core.factory.handlers.CustomOriginExistCheck;
-import me.dueris.genesismc.core.factory.powers.block.MasterWebs;
+import me.dueris.genesismc.core.factory.powers.block.Webbing;
 import me.dueris.genesismc.core.factory.powers.block.fluid.WaterDamage;
-import me.dueris.genesismc.core.factory.powers.entity.Arthropod;
-import me.dueris.genesismc.core.factory.powers.entity.FreshAir;
-import me.dueris.genesismc.core.factory.powers.entity.LayEggs;
-import me.dueris.genesismc.core.factory.powers.entity.Reach;
+import me.dueris.genesismc.core.factory.powers.entity.*;
 import me.dueris.genesismc.core.factory.powers.food.Carnivore;
 import me.dueris.genesismc.core.factory.powers.food.Vegitarian;
 import me.dueris.genesismc.core.factory.powers.runnables.BurningWrath;
 import me.dueris.genesismc.core.factory.powers.runnables.Climbing;
+import me.dueris.genesismc.core.factory.powers.runnables.NoCobwebSlowdown;
+import me.dueris.genesismc.core.factory.powers.runnables.SlowFalling;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -28,17 +26,22 @@ public class OriginStartHandler {
         BurningWrath burningWrath = new BurningWrath();
         burningWrath.runTaskTimer(GenesisMC.getPlugin(), 0, 5);
 
+        SlowFalling slowFalling = new SlowFalling();
+        slowFalling.runTaskTimer(GenesisMC.getPlugin(), 0, 5);
+
     }
 
     public static void StartListeners(){
         getServer().getPluginManager().registerEvents(new CustomOriginExistCheck(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new Powers(), GenesisMC.getPlugin());
-        getServer().getPluginManager().registerEvents(new MasterWebs(), GenesisMC.getPlugin());
+        getServer().getPluginManager().registerEvents(new Webbing(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new Arthropod(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new Carnivore(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new LayEggs(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new Vegitarian(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new FreshAir(), GenesisMC.getPlugin());
         getServer().getPluginManager().registerEvents(new Reach(), GenesisMC.getPlugin());
+        //getServer().getPluginManager().registerEvents(new NoCobwebSlowdown(), GenesisMC.getPlugin());
+        getServer().getPluginManager().registerEvents(new HotHands(), GenesisMC.getPlugin());
     }
 }
