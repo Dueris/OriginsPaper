9bd1f3aa365bc9825c8ff6abecacd9a4c3952e58
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-14 14:41:08 -0700

    added development api

--- src/main/java/me/dueris/genesismc/core/api/OriginAPI.java
+++ src/main/java/me/dueris/genesismc/core/api/OriginAPI.java
@@ -1,4 +1,35 @@
 package me.dueris.genesismc.core.api;
 
+import me.dueris.genesismc.core.api.factory.CustomOriginAPI;
+
+import java.util.ArrayList;
+
+import static me.dueris.genesismc.core.api.factory.CustomOriginAPI.customOrigins;
+
 public class OriginAPI {
+    
+    public static ArrayList<String> getLoadedOrigins(){
+        customOrigins.put("genesis:origin-human", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-enderian", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-merling", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-phantom", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-elytrian", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-blazeborn", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-avian", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-arachnid", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-shulk", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-feline", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-starborne", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-allay", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-rabbit", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-bee", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-human", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-sculkling", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-creep", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-slimeling", "GenesisMC-Core");
+        customOrigins.put("genesis:origin-piglin", "GenesisMC-Core");
+        return new ArrayList<>(customOrigins.keySet());
+    }
+    
+    
 }
--- src/main/java/me/dueris/genesismc/core/api/entity/OriginPlayer.java
+++ src/main/java/me/dueris/genesismc/core/api/entity/OriginPlayer.java
@@ -1,6 +1,8 @@
 package me.dueris.genesismc.core.api.entity;
 
 import me.dueris.genesismc.core.GenesisMC;
+import me.dueris.genesismc.core.choosing.contents.origins.ExpandedOriginContent;
+import me.dueris.genesismc.core.choosing.contents.origins.OriginalOriginContent;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -9,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class OriginPlayer {
 
-    public static boolean hasOrigin(Player player){
+    public static boolean hasChosenOrigin(Player player){
         PersistentDataContainer data = player.getPersistentDataContainer();
         @Nullable String origintag = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
         if(origintag.equalsIgnoreCase("")) {return false;}
@@ -18,5 +20,73 @@ public class OriginPlayer {
         }
     }
 
+    public static boolean hasOrigin(Player player, String origintag){
+        PersistentDataContainer data = player.getPersistentDataContainer();
+        @Nullable String origin = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
+        if(origin.equalsIgnoreCase("")) return false;
+        if(origin.contains(origintag)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String getOriginTag(Player player){
+        PersistentDataContainer data = player.getPersistentDataContainer();
+        @Nullable String origintag = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
+        return origintag;
+    }
+
+    public static void removeOrigin(Player player){
+        PersistentDataContainer data = player.getPersistentDataContainer();
+        data.set(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING, "");
+    }
+
+    public static boolean hasCoreOrigin(Player player){
+        PersistentDataContainer data = player.getPersistentDataContainer();
+        @Nullable String origintagPlayer = data.get(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING);
+        if(origintagPlayer.contains("genesis:origin-human")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-enderian")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-merling")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-phantom")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-elytrian")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-blazeborn")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-avian")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-arachnid")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-shulk")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-feline")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-starborne")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-allay")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-rabbit")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-bee")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-sculkling")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-creep")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-slimeling")){
+            return true;
+        }else if(origintagPlayer.contains("genesis:origin-piglin")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void setOrigin(Player player, String origin){
+        player.getPersistentDataContainer().set(new NamespacedKey(GenesisMC.getPlugin(), "origintag"), PersistentDataType.STRING, origin);
+    }
 
 }
