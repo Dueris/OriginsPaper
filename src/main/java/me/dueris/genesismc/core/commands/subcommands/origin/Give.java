6d178e5b6d6cd2982c25bd1c580fa37570bc45ff
Mapty231 <tye.exe@gmail.com>
2023-05-17 18:01:15 +0100

    Finished give command implementation.

--- src/main/java/me/dueris/genesismc/core/commands/subcommands/origin/Give.java
+++ src/main/java/me/dueris/genesismc/core/commands/subcommands/origin/Give.java
@@ -2,12 +2,14 @@ package me.dueris.genesismc.core.commands.subcommands.origin;
 
 import me.dueris.genesismc.core.commands.subcommands.SubCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Random;
+
+import static me.dueris.genesismc.core.items.OrbOfOrigins.orb;
+import static org.bukkit.ChatColor.RED;
 
 public class Give extends SubCommand {
     @Override
@@ -26,26 +28,36 @@ public class Give extends SubCommand {
     @Override
     public void perform(Player p, String[] args) {
         ArrayList<Player> players = new ArrayList<>();
+
         if (args[1].equals("@a")) {
             players.addAll(Bukkit.getOnlinePlayers());
+
         } else if (args[1].equals("@e")) {
-            p.sendMessage("not done yet, please come back later");
+            p.sendMessage(RED + "Only player may be affected by this command, but the provided selector includes entities.");
+            return;
+
         } else if (args[1].equals("@p")) {
-            //for (int i = 0; players.size() < 1; i++)
-                //players.add(Objects.requireNonNull(Bukkit.getPlayer(p.getLocation().getNearbyPlayers(i).toString())).getName());
-            p.sendMessage("not done yet, please come back later");
+            for (int i = 0; players.size() < 1; i++) {
+                if (p.getLocation().getNearbyPlayers(i).size() == 0) continue;
+                CraftPlayer craftPlayer = (CraftPlayer) p.getLocation().getNearbyPlayers(i).toArray()[0];
+                players.add(Bukkit.getPlayer(craftPlayer.getName()));
+            }
+
         } else if (args[1].equals("@r")) {
             Random random = new Random();
             int randomInt = random.nextInt(Bukkit.getOnlinePlayers().size());
             players.add((Player) Bukkit.getOnlinePlayers().toArray()[randomInt]);
+
         } else if (args[1].equals("@s")) {
             players.add(p);
         } else {
-            try {
-                players.add(Bukkit.getPlayer(args[1]));
-            } catch (Exception e) {}
+            try {players.add(Bukkit.getPlayer(args[1]));}
+            catch (Exception e) {p.sendMessage(RED + "Player not found!");}
         }
 
-        p.sendMessage(players.toString());
+
+        for (Player player : players) {
+            if (args[2].equals("genesis:orb_of_orig")) player.getInventory().addItem(orb);
+        }
     }
 }
