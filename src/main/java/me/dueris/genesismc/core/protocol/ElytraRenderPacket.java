5dbacc71318f0822e32e30c1c253e92ce664e1b8
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-21 12:31:32 -0700

    added ElytraRenderPacket and setup custom channel

---
+++ src/main/java/me/dueris/genesismc/core/protocol/ElytraRenderPacket.java
@@ -0,0 +1,42 @@
+package me.dueris.genesismc.core.protocol;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import net.minecraft.resources.ResourceLocation;
+import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import oshi.hardware.platform.unix.freebsd.FreeBsdUsbDevice;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ElytraRenderPacket {
+    public static void sendCustomPacket(Player player, String message) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        try {
+            // Write packet data
+            dataOutputStream.writeUTF(message);
+
+            // Create the custom packet
+            byte[] packetData = outputStream.toByteArray();
+            ClientboundCustomPayloadPacket packet = new ClientboundCustomPayloadPacket(ResourceLocation.tryParse("genesis-packet-channel"), new FriendlyByteBuf(Unpooled.wrappedBuffer(packetData)));
+
+            // Send the packet to the player
+            ((CraftPlayer) player).getHandle().connection.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close streams
+            try {
+                dataOutputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
