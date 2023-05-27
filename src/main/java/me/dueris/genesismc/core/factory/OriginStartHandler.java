2c631cf88dd1e4f8bdf2f61d05215957f1bf5a8e
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-26 20:16:58 -0700

    attempted a holding mechanisem for climbing

--- src/main/java/me/dueris/genesismc/core/factory/OriginStartHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/OriginStartHandler.java
@@ -132,6 +132,7 @@ public class OriginStartHandler {
         getServer().getPluginManager().registerEvents(new LaunchAir(), getPlugin());
         getServer().getPluginManager().registerEvents(new GoldAmourBellow(), getPlugin());
         getServer().getPluginManager().registerEvents(new ArielCombat(), getPlugin());
+        getServer().getPluginManager().registerEvents(new Climbing(), getPlugin());
 
     }
 }
