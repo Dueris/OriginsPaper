23dec2375c4768d5dd930caaba1f2d18de215f95
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-27 15:59:36 -0700

    added OriginDataType

---
+++ src/main/java/me/dueris/genesismc/core/api/enums/OriginDataType.java
@@ -0,0 +1,12 @@
+package me.dueris.genesismc.core.api.enums;
+
+import org.bukkit.inventory.ItemStack;
+
+public enum OriginDataType {
+    ORIGINTAG,
+    PHANTOMIZED_ID,
+    TOGGLE,
+    CAN_EXPLODE,
+    IN_PHANTOMIZED_FORM,
+    SHULKER_BOX_DATA
+}
