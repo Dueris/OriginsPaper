9925ff9bd0b545e45801ab5d4bbf830208909ac3
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-28 20:38:33 -0700

    fixed the entire waterbreathe system breakin

--- src/main/java/me/dueris/genesismc/core/factory/powers/block/fluid/WaterBreathe.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/block/fluid/WaterBreathe.java
@@ -51,7 +51,8 @@ public class WaterBreathe extends BukkitRunnable {
     public static boolean isInBreathableWater(Player player) {
         Block block = player.getEyeLocation().getBlock();
         Material material = block.getType();
-        if(block.isLiquid() || block.getState() instanceof Waterlogged || block.getBlockData() instanceof Waterlogged || !block.isCollidable()) return true;
+        if(!block.isCollidable() && !block.getType().equals(Material.AIR) && !block.getType().equals(Material.LAVA)) {return true;}
+        if(block.getType().equals(Material.WATER) /* || !block.isCollidable() */ ) {return true;}
         return false;
     }
 
