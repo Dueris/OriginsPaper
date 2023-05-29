82f0bbd8c1d6c0df030c0644976d50e49cdae98a
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-05-28 20:40:55 -0700

    removed bugfix bc of signs

--- src/main/java/me/dueris/genesismc/core/factory/powers/block/fluid/WaterBreathe.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/block/fluid/WaterBreathe.java
@@ -51,7 +51,7 @@ public class WaterBreathe extends BukkitRunnable {
     public static boolean isInBreathableWater(Player player) {
         Block block = player.getEyeLocation().getBlock();
         Material material = block.getType();
-        if(!block.isCollidable() && !block.getType().equals(Material.AIR) && !block.getType().equals(Material.LAVA)) {return true;}
+        //if(!block.isCollidable() && !block.getType().equals(Material.AIR) && !block.getType().equals(Material.LAVA) && !block.getType().isBlock()) {return true;}
         if(block.getType().equals(Material.WATER) /* || !block.isCollidable() */ ) {return true;}
         return false;
     }
