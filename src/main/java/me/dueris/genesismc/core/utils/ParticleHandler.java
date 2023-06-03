e84746d9c524dc3095a59fd19c8aea1ea2dc8193
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-03 12:16:54 -0700

    added flame_particles

--- src/main/java/me/dueris/genesismc/core/utils/ParticleHandler.java
+++ src/main/java/me/dueris/genesismc/core/utils/ParticleHandler.java
@@ -7,19 +7,18 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import static me.dueris.genesismc.core.factory.powers.Powers.ender_particles;
+import static me.dueris.genesismc.core.factory.powers.Powers.flame_particles;
 
 public class ParticleHandler extends BukkitRunnable {
     @Override
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (p.getGameMode() != GameMode.SPECTATOR && !p.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
-                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (ender_particles.contains(OriginPlayer.getOrigin(p).getTag())) {
                     Particle.DustTransition ender_particle = new Particle.DustTransition(Color.fromRGB(252, 0, 255), Color.fromRGB(209, 0, 255), 0.5F);
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getLocation(), 2, 0.6F, -0.5, 0.6F, ender_particle);
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getEyeLocation(), 2, 0.6F, -0.5, 0.6F, ender_particle);
                 }
-                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (OriginPlayer.getOrigin(p).getTag().contains("genesis:origin-starborne")) {
                     Location eyelocation = new Location(p.getWorld(), p.getEyeLocation().getX(), p.getEyeLocation().getY() - 0.3, p.getEyeLocation().getZ());
                     p.getWorld().spawnParticle(Particle.ELECTRIC_SPARK, p.getLocation(), 2, 0.25F, -0.5, 0.25F, 0);
@@ -29,15 +28,15 @@ public class ParticleHandler extends BukkitRunnable {
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, eyelocation, 2, 0.25F, -0.5, 0.25F, dustColorTransition);
 
                 }
-                //origintag.equalsIgnoreCase("genesis:origin-")
                 if (OriginPlayer.getOrigin(p).getTag().contains("genesis:origin-allay")) {
                     Particle.DustTransition dustColorTransition = new Particle.DustTransition(Color.fromRGB(97, 255, 250), Color.fromRGB(163, 255, 244), 0.275F);
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getLocation(), 3, 0.4F, -0.5, 0.4F, dustColorTransition);
                     p.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, p.getEyeLocation(), 3, 0.4F, -0.5, 0.4F, dustColorTransition);
-
-
                 }
-
+                if(flame_particles.contains(OriginPlayer.getOrigin(p).getTag())){
+                    p.getWorld().spawnParticle(Particle.FLAME, p.getLocation(), 3,0.4F, -0.5, 0.4F);
+                    p.getWorld().spawnParticle(Particle.FLAME, p.getEyeLocation(), 3,0.4F, -0.5, 0.4F);
+                }
             }
         }
     }
