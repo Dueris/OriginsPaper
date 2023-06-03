e96659d72eacc3ccf0e95fcb304880e24bd1fb66
Mapty231 <tye.exe@gmail.com>
2023-06-03 18:25:12 +0100

    Added javadocs

--- src/main/java/me/dueris/genesismc/core/utils/OriginContainer.java
+++ src/main/java/me/dueris/genesismc/core/utils/OriginContainer.java
@@ -12,6 +12,14 @@ public class OriginContainer implements Serializable {
     HashMap<String, Object> originFile;
     ArrayList<PowerContainer> powerContainer;
 
+    /**
+     * The files are parsed into hashmaps by putting the json key as the key and the value as the value.
+     *
+     * @param tag The origin tag.
+     * @param originLayerFile The origin layer file, parsed into a HashMap.
+     * @param originFile The origin file, parsed into a HashMap.
+     * @param powerContainer An array of powers that the origin has.
+     */
     public OriginContainer(String tag, HashMap<String, Object> originLayerFile, HashMap<String, Object> originFile, ArrayList<PowerContainer> powerContainer) {
         this.tag = tag;
         this.originLayerFile = originLayerFile;
@@ -24,46 +32,69 @@ public class OriginContainer implements Serializable {
      */
     @Override
     public String toString() {
-        return "Tag: " + this.tag + ", OriginLayerFile: " + this.originLayerFile + ", OriginFile: " + this.originFile + ", PowerContainer {" + this.powerContainer.toString() + "}";
+        return "Tag: " + this.tag + ", OriginLayerFile: " + this.originLayerFile + ", OriginFile: " + this.originFile + ", PowerContainer: " + this.powerContainer.toString();
     }
 
-
+    /**
+     * @return The origin tag.
+     */
     public String getTag() {
         return this.tag;
     }
 
+    /**
+     * @return The origin layer file parsed into a HashMap.
+     */
     public HashMap<String, Object> getOriginLayerFile() {
         return new  HashMap<String, Object>(this.originLayerFile);
     }
 
+    /**
+     * @return The origin file parsed into a HashMap.
+     */
     public HashMap<String, Object> getOriginFile() {
         return new HashMap<String, Object>(this.originFile);
     }
 
+    /**
+     * @return An array containing all the origin powers.
+     */
     public ArrayList<PowerContainer> getPowerContainers() {
         return new ArrayList<PowerContainer>(this.powerContainer);
     }
 
 
     //origin file
+    /**
+     * @return The name of the origin.
+     */
     public String getName() {
         String name = (String) this.originFile.get("name");
         if (name == null) return "No Name";
         return name;
     }
 
+    /**
+     * @return The description for the origin.
+     */
     public String getDescription() {
         String description = (String) this.originFile.get("description");
         if (description == null) return "No Description";
         return description;
     }
 
+    /**
+     * @return An array of powers from the origin.
+     */
     public ArrayList<String> getPowers() {
         ArrayList<String> powers = (ArrayList<String>) this.originFile.get("powers");
         if (powers == null) return new ArrayList<>();
         return powers;
     }
 
+    /**
+     * @return The icon of the origin.
+     */
     public String getIcon() {
         Object value = this.originFile.get("icon");
         try {
@@ -79,12 +110,18 @@ public class OriginContainer implements Serializable {
         }
     }
 
+    /**
+     * @return The impact of the origin.
+     */
     public Long getImpact() {
         Long impact = (Long) this.originFile.get("impact");
         if (impact == null) return 1L;
         return impact;
     }
 
+    /**
+     * @return If the origin is choose-able from the choose menu.
+     */
     public Boolean getUnchooseable() {
         Boolean hidden = (Boolean) this.originFile.get("unchoosable");
         if (hidden == null) return false;
