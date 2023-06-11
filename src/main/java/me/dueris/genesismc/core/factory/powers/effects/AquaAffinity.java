af299fe58c5090074bb51854a1127f0d9f352e1a
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-11 16:33:28 -0700

    add aqua_affinity power

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/effects/AquaAffinity.java
@@ -0,0 +1,23 @@
+package me.dueris.genesismc.core.factory.powers.effects;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static me.dueris.genesismc.core.factory.powers.Powers.aqua_affinity;
+import static me.dueris.genesismc.core.factory.powers.block.fluid.WaterBreathe.isInBreathableWater;
+
+public class AquaAffinity extends BukkitRunnable {
+    @Override
+    public void run() {
+        for(Player p : Bukkit.getOnlinePlayers()){
+            if(aqua_affinity.contains(p)){
+                if(isInBreathableWater(p)){
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 15, 3, false, false, false));
+                }
+            }
+        }
+    }
+}
