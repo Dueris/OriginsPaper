5eaefdd4b7295aa8104122bae6ff3d370eed27c2
Mapty231 <tye.exe@gmail.com>
2023-06-13 19:30:14 +0100

    Added AQUA

--- src/main/java/me/dueris/genesismc/core/utils/Colours.java
+++ src/main/java/me/dueris/genesismc/core/utils/Colours.java
@@ -3,4 +3,5 @@ package me.dueris.genesismc.core.utils;
 public class Colours {
     public static String RED = "#fb5454";
     public static String YELLOW = "#fbfb54";
+    public static String AQUA = "#54fbfb";
 }
