dfff730742353a6dac86dc96d3abd8f6b6ee58bc
Mapty231 <tye.exe@gmail.com>
2023-06-14 12:18:06 +0100

    updated null origin check

--- src/main/java/me/dueris/genesismc/core/choosing/ChoosingForced.java
+++ src/main/java/me/dueris/genesismc/core/choosing/ChoosingForced.java
@@ -1,6 +1,7 @@
 package me.dueris.genesismc.core.choosing;
 
 import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.CraftApoli;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -13,7 +14,7 @@ public class ChoosingForced extends BukkitRunnable {
     @Override
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if (OriginPlayer.getOrigin(p).getTag().equalsIgnoreCase("genesis:origin-choosing") || OriginPlayer.getOrigin(p).getTag().equalsIgnoreCase("genesis:origin-null")) {
+            if (OriginPlayer.getOrigin(p).getTag().equals(new CraftApoli().nullOrigin().getTag())) {
                 if (!p.getOpenInventory().getTitle().equalsIgnoreCase("Choosing Menu") && !p.getOpenInventory().getTitle().equalsIgnoreCase("Custom Origins") && !p.getOpenInventory().getTitle().equalsIgnoreCase("Expanded Origins") && !p.getOpenInventory().getTitle().equalsIgnoreCase("Custom Origin") && !p.getOpenInventory().getTitle().equalsIgnoreCase("Origin")) {
                     @NotNull Inventory mainmenu = Bukkit.createInventory(p, 54, "Choosing Menu");
                     mainmenu.setContents(GenesisMainMenuContents(p));
