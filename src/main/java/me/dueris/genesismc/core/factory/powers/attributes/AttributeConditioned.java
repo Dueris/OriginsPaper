2c71aec0fda169ae55300aa9b0050f087c029e23
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-17 16:27:21 -0700

    removed debug code

--- src/main/java/me/dueris/genesismc/core/factory/powers/attributes/AttributeConditioned.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/attributes/AttributeConditioned.java
@@ -42,8 +42,6 @@ public class AttributeConditioned implements Listener {
         operationMap.put("multiply_random_max", (a, b) -> a * random.nextInt(b));
         operationMap.put("divide_random_max", (a, b) -> a / random.nextInt(b));
 
-        System.out.println(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier());
-
         Attribute attribute_modifier = Attribute.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier().get("attribute").toString().split(":")[1].replace(".", "_").toUpperCase());
         int value = Integer.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier().get("value").toString());
         String operation = String.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier().get("operation"));
@@ -76,8 +74,6 @@ public class AttributeConditioned implements Listener {
         operationMap.put("subtract_random_max", (a, b) -> a + random.nextInt(b));
         operationMap.put("multiply_random_max", (a, b) -> a / random.nextInt(b));
         operationMap.put("divide_random_max", (a, b) -> a * random.nextInt(b));
-
-        System.out.println(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier());
 
         Attribute attribute_modifier = Attribute.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier().get("attribute").toString().split(":")[1].replace(".", "_").toUpperCase());
         int value = Integer.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:conditioned_attribute").getModifier().get("value").toString());
--- src/main/java/me/dueris/genesismc/core/factory/powers/attributes/AttributeHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/attributes/AttributeHandler.java
@@ -48,8 +48,6 @@ public class AttributeHandler extends BukkitRunnable implements Listener {
             operationMap.put("multiply_random_max", (a, b) -> a * random.nextInt(b));
             operationMap.put("divide_random_max", (a, b) -> a / random.nextInt(b));
 
-            System.out.println(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:attribute").getModifier());
-
             Attribute attribute_modifier = Attribute.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:attribute").getModifier().get("attribute").toString().split(":")[1].replace(".", "_").toUpperCase());
             int value = Integer.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:attribute").getModifier().get("value").toString());
             String operation = String.valueOf(OriginPlayer.getOrigin(p).getPowerFileFromType("origins:attribute").getModifier().get("operation"));
