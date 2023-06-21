a24a75d3c5def0aba7cd19efd32f1c6d74b6ebef
Zeki <iziquizel@gmail.com>
2023-06-21 11:59:31 -0300

    Fixed new players not receiving a null origin resulting in NullPointerExceptions

--- src/main/java/me/dueris/genesismc/core/JoiningHandler.java
+++ src/main/java/me/dueris/genesismc/core/JoiningHandler.java
@@ -37,9 +37,11 @@ public class JoiningHandler implements Listener {
                 if (("origin-" + (origin.getTag().substring(8))).equals(originTag.substring(8)))
                     p.getPersistentDataContainer().set(new NamespacedKey(GenesisMC.getPlugin(), "origins"), PersistentDataType.BYTE_ARRAY, CraftApoli.toByteArray(new HashMap<>(Map.of("origins:origin", origin))));
             }
+        } else {
+            p.getPersistentDataContainer().set(new NamespacedKey(GenesisMC.getPlugin(), "origins"), PersistentDataType.BYTE_ARRAY, CraftApoli.toByteArray(new HashMap<>(Map.of("origins:origin", CraftApoli.nullOrigin()))));
         }
 
-        ByteArrayInputStream bis = new ByteArrayInputStream(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "origin"), PersistentDataType.BYTE_ARRAY));
+        ByteArrayInputStream bis = new ByteArrayInputStream(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "origins"), PersistentDataType.BYTE_ARRAY));
         try {
             ObjectInput oi = new ObjectInputStream(bis);
             OriginContainer origin = (OriginContainer) oi.readObject();
