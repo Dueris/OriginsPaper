800d67cab1faf80039857d59f5ec51c785790608
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-21 19:55:36 -0700

    fixed bug thingy with flight

--- src/main/java/me/dueris/genesismc/core/factory/powers/world/FlightHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/world/FlightHandler.java
@@ -10,10 +10,14 @@ import static me.dueris.genesismc.core.factory.powers.Powers.creative_flight;
 public class FlightHandler extends BukkitRunnable {
     @Override
     public void run() {
-        for(Player p : Bukkit.getOnlinePlayers()){
-            if(p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR) || creative_flight.contains(p)){
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (creative_flight.contains(p)) {
                 p.setAllowFlight(true);
-            }else{p.setAllowFlight(false);}
+            } else {
+                if (p.getGameMode().equals(GameMode.CREATIVE)) {
+                    p.setAllowFlight(true);
+                } else p.setAllowFlight(p.getGameMode().equals(GameMode.SPECTATOR));
+            }
         }
     }
 }
