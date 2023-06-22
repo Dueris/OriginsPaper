c09176c0960c72b32b6379b21912c17fa212e27d
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-22 15:51:53 -0700

    added toggle_ngiht_vision array

--- src/main/java/me/dueris/genesismc/core/factory/powers/Powers.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/Powers.java
@@ -100,6 +100,7 @@ public class Powers implements Listener {
     public static ArrayList<Player> stacking_status_effect = new ArrayList<>();
     public static ArrayList<Player> starting_equip = new ArrayList<>();
     public static ArrayList<Player> swimming = new ArrayList<>();
+    public static ArrayList<Player> toggle_night_vision = new ArrayList<>();
 
     //genesis
     public static ArrayList<Player> hot_hands = new ArrayList<>();
