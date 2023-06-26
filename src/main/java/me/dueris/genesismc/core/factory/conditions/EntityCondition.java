b9321b25d06255db2a29877c7d847c70278810a4
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-26 12:29:33 -0700

    i stopped bc i am confused for the system

--- src/main/java/me/dueris/genesismc/core/factory/conditions/EntityCondition.java
+++ src/main/java/me/dueris/genesismc/core/factory/conditions/EntityCondition.java
@@ -8,16 +8,36 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class EntityCondition {
     public static boolean checkEntityCondition(Player p, OriginContainer origin, String powerfile, Entity entity) {
-        String entity_con_type = origin.getPowerFileFromType(powerfile).getEntityConditionFromDamageCondition().get("type").toString();
-        String entity_type = origin.getPowerFileFromType(powerfile).getEntityConditionFromDamageCondition().get("entity_type").toString();
-        if(entity_con_type.equalsIgnoreCase("origins:entity_type")){
-            EntityType entityType = EntityType.valueOf(entity_type.split(":")[1].toUpperCase());
-            if(entity.getType().equals(entityType)){
-                return true;
+        try{
+            String entity_con_type = origin.getPowerFileFromType(powerfile).getEntityConditionFromDamageCondition().get("type").toString();
+            String entity_type = origin.getPowerFileFromType(powerfile).getEntityConditionFromDamageCondition().get("entity_type").toString();
+            if(entity_con_type.equalsIgnoreCase("origins:entity_type")){
+                EntityType entityType = EntityType.valueOf(entity_type.split(":")[1].toUpperCase());
+                if(entity.getType().equals(entityType)){
+                    return true;
+                }
             }
-        }
-        if(entity_con_type.equalsIgnoreCase("origins:ability")){
-            String ability = origin.getPowerFileFromType(powerfile);
+            if(entity_con_type.equalsIgnoreCase("origins:ability")){
+                try{
+                    String ability = origin.getPowerFileFromType(powerfile).getEntityConditionFromAnywhere().get("ability").toString();
+                    if(ability.equalsIgnoreCase("minecraft:flying")){
+                        if(entity instanceof Player){
+                            return ((Player) entity).isGliding() || ((Player) entity).isFlying();
+                        }
+
+                    }
+                    if(ability.equalsIgnoreCase("instabuild")){
+                        if(entity instanceof Player){
+                            Player player = (Player) entity;
+                        }
+                    }
+                }catch (Exception e){
+                    return false;
+                }
+
+            }
+        }catch (Exception e){
+            return false;
         }
         return false;
     }
