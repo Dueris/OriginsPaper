9f55679100f104dbf5cabfaccfe700d04c5a8dbb
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-27 10:11:20 -0700

    added minecraft:flying check for origins:ability

--- src/main/java/me/dueris/genesismc/core/factory/conditions/entity/EntityCondition.java
+++ src/main/java/me/dueris/genesismc/core/factory/conditions/entity/EntityCondition.java
@@ -8,8 +8,14 @@ public class EntityCondition {
     public static boolean check(Player p, OriginContainer origin, String powerfile, Entity entity){
         String type = origin.getPowerFileFromType(powerfile).getEntityCondition().get("type").toString();
         if(type.equalsIgnoreCase("origins:ability")){
-            p.sendMessage("dfs");
-            return true;
+            String ability = origin.getPowerFileFromType(powerfile).getEntityCondition().get("ability").toString();
+            if(ability.equalsIgnoreCase("minecraft:flying")){
+                if(entity instanceof Player){
+                    Player player = (Player) entity;
+                    if(player.isFlying()) return true;
+                }
+            }
+
         }
         return false;
     }
