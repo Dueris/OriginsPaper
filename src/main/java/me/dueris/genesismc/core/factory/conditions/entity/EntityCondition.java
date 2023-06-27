cdd941f86f2d201d4d812c3227085576752d6000
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-06-27 09:35:46 -0700

    modified check reqiureemtns

--- src/main/java/me/dueris/genesismc/core/factory/conditions/entity/EntityCondition.java
+++ src/main/java/me/dueris/genesismc/core/factory/conditions/entity/EntityCondition.java
@@ -1,11 +1,16 @@
 package me.dueris.genesismc.core.factory.conditions.entity;
 
 import me.dueris.genesismc.core.utils.OriginContainer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 public class EntityCondition {
-    public static boolean check(Player p, OriginContainer origin, String powerfile){
+    public static boolean check(Player p, OriginContainer origin, String powerfile, Entity entity){
         String type = origin.getPowerFileFromType(powerfile).getEntityCondition().get("type").toString();
+        if(type.equalsIgnoreCase("origins:ability")){
+            p.sendMessage("dfs");
+            return true;
+        }
         return false;
     }
 }
