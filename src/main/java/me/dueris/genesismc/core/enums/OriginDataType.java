e5a6d836e6a19191a052988099ab6ce86130b069
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-06 10:48:49 -0700

    modified enum of OriginDataType

--- src/main/java/me/dueris/genesismc/core/enums/OriginDataType.java
+++ src/main/java/me/dueris/genesismc/core/enums/OriginDataType.java
@@ -2,9 +2,8 @@ package me.dueris.genesismc.core.enums;
 
 public enum OriginDataType {
     ORIGINTAG,
-    PHANTOMIZED_ID,
     TOGGLE,
     CAN_EXPLODE,
-    IN_PHANTOMIZED_FORM,
+    IN_PHASING_FORM,
     SHULKER_BOX_DATA
 }
