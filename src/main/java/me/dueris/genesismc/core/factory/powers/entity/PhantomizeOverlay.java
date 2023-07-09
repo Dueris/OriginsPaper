7ac8aef9b43055698bdc1e0b6c9f8bc47f126255
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-09 11:15:05 -0700

    commented out old files

--- src/main/java/me/dueris/genesismc/core/factory/powers/entity/PhantomizeOverlay.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/entity/PhantomizeOverlay.java
@@ -10,23 +10,23 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import static me.dueris.genesismc.core.factory.powers.Powers.phantomize_overlay;
 
-public class PhantomizeOverlay extends BukkitRunnable {
-    @Override
-    public void run() {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-
-            PersistentDataContainer data = p.getPersistentDataContainer();
-            int phantomid = data.get(new NamespacedKey(GenesisMC.getPlugin(), "in-phantomform"), PersistentDataType.INTEGER);
-
-            if (phantomid == 2) {
-                if (phantomize_overlay.contains(p)) {
-                    Phantomized.initializePhantomOverlay(p);
-                } else {
-                    Phantomized.deactivatePhantomOverlay(p);
-                }
-            } else {
-                Phantomized.deactivatePhantomOverlay(p);
-            }
-        }
-    }
-}
+//public class PhantomizeOverlay extends BukkitRunnable {
+////    @Override
+////    public void run() {
+////        for (Player p : Bukkit.getOnlinePlayers()) {
+////
+////            PersistentDataContainer data = p.getPersistentDataContainer();
+////            int phantomid = data.get(new NamespacedKey(GenesisMC.getPlugin(), "in-phantomform"), PersistentDataType.INTEGER);
+////
+////            if (phantomid == 2) {
+////                if (phantomize_overlay.contains(p)) {
+////                    Phantomized.initializePhantomOverlay(p);
+////                } else {
+////                    Phantomized.deactivatePhantomOverlay(p);
+////                }
+////            } else {
+////                Phantomized.deactivatePhantomOverlay(p);
+////            }
+////        }
+////    }
+////}
