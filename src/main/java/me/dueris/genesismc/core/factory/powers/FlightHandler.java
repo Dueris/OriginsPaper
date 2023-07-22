476c50b126919122a419c7ada1a383c4413c83d8
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-21 19:11:21 -0700

    fixed FlightHandling

--- src/main/java/me/dueris/genesismc/core/factory/powers/FlightHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/FlightHandler.java
@@ -15,10 +15,12 @@ public class FlightHandler extends BukkitRunnable {
     @Override
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN) != null || Boolean.TRUE.equals(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN))){
-                if(p.getAllowFlight() == true){
+            if(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN) != null && Boolean.TRUE.equals(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN))){
                     p.setFlying(true);
-                }
+            } else {
+                if (p.getGameMode().equals(GameMode.CREATIVE)) {
+                    p.setAllowFlight(true);
+                } else p.setAllowFlight(p.getGameMode().equals(GameMode.SPECTATOR));
             }
             if (creative_flight.contains(p) || OriginPlayer.isInPhantomForm(p)) {
                 p.setAllowFlight(true);
@@ -26,6 +28,12 @@ public class FlightHandler extends BukkitRunnable {
                 if (p.getGameMode().equals(GameMode.CREATIVE)) {
                     p.setAllowFlight(true);
                 } else p.setAllowFlight(p.getGameMode().equals(GameMode.SPECTATOR));
+            }
+
+            if(p.getEyeLocation().getBlock().isCollidable()){
+                p.getPersistentDataContainer().set(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN, true);
+            }else{
+                p.getPersistentDataContainer().set(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN, false);
             }
         }
     }
