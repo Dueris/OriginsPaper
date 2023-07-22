0aa937e5e7a5ff5e09595deb3648444e94131e2d
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-21 17:28:18 -0700

    ok.. this plugin is unstable now.. by a lot

--- src/main/java/me/dueris/genesismc/core/factory/powers/FlightHandler.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/FlightHandler.java
@@ -16,7 +16,9 @@ public class FlightHandler extends BukkitRunnable {
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN) != null || Boolean.TRUE.equals(p.getPersistentDataContainer().get(new NamespacedKey(GenesisMC.getPlugin(), "insideBlock"), PersistentDataType.BOOLEAN))){
-                p.setFlying(true);
+                if(p.getAllowFlight() == true){
+                    p.setFlying(true);
+                }
             }
             if (creative_flight.contains(p) || OriginPlayer.isInPhantomForm(p)) {
                 p.setAllowFlight(true);
