ecd5b0cfe06412b6ee9275a0c2967d02253b2a28
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-21 20:56:31 -0700

    added origins:effect_immunity

--- src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/effects/EffectImmunity.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/effects/EffectImmunity.java
@@ -3,7 +3,9 @@ package me.dueris.genesismc.core.factory.powers.OriginsMod.effects;
 import me.dueris.genesismc.core.entity.OriginPlayer;
 import me.dueris.genesismc.core.utils.OriginContainer;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -16,19 +18,42 @@ public class EffectImmunity extends BukkitRunnable {
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (effect_immunity.contains(p)) {
-//                for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
-//                    List<String> effectStrings = origin.getPowerFileFromType("origins:effect_immunity").getEffects();
-//                    for(String effectString : effectStrings){
-//                        PotionEffectType effectType = PotionEffectType.getByName(effectString);
-//                        if(effectType != null){
-//                            if(p.hasPotionEffect(effectType)){
-//                                p.removePotionEffect(effectType);
-//                            }
-//                        }
-//                    }
-//                }
+                for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
 
+                    if(origin.getPowerFileFromType("origins:effect_immunity").getEffect() != null){
+                        PotionEffectType effectType = PotionEffectType.getByName(origin.getPowerFileFromType("origins:effect_immunity").getEffect().toString());
+                        if(origin.getPowerFileFromType("origins:effect_immunity").isInverted()){
+                            if(effectType != null){
+                                if(p.hasPotionEffect(effectType)){
+                                    p.removePotionEffect(effectType);
+                                }
+                            } else {
+                                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] UNABLE TO GET PowerEffectType ENUM FOR POWER origins:effect_immunity");
+                            }
+                        } else {
+                            if(effectType != null){
+                                if(!p.hasPotionEffect(effectType)){
+                                    p.addPotionEffect(new PotionEffect(effectType, 25, 1));
+                                }
+                            } else {
+                                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] UNABLE TO GET PowerEffectType ENUM FOR POWER origins:effect_immunity");
+                            }
+                        }
 
+                    } else if (origin.getPowerFileFromType("origins:effect_immunity").getEffects() != null) {
+                        List<String> effectStrings = origin.getPowerFileFromType("origins:effect_immunity").getEffects();
+                        for(String effectString : effectStrings){
+                            PotionEffectType effectType = PotionEffectType.getByName(effectString);
+                            if(effectType != null){
+                                if(p.hasPotionEffect(effectType)){
+                                    p.removePotionEffect(effectType);
+                                }
+                            }
+                        }
+                    } else {
+                        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[GenesisMC] UNABLE TO GET EFFECT FOR POWER origins:effect_immunity");
+                    }
+                }
             }
         }
     }
