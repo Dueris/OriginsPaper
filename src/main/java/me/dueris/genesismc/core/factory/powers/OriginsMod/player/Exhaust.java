ebc253bbd74d2872af4058e936b73330fd6f664c
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-22 13:13:34 -0700

    added origins:exhaust

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/player/Exhaust.java
@@ -0,0 +1,52 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.player;
+
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import me.dueris.genesismc.core.utils.PowerContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static me.dueris.genesismc.core.factory.powers.Powers.burn;
+import static me.dueris.genesismc.core.factory.powers.Powers.more_exhaustion;
+
+public class Exhaust extends BukkitRunnable {
+
+    private Long interval;
+
+    private int ticksE;
+
+    public Exhaust() {
+        this.interval = 1L;
+        this.ticksE = 0;
+    }
+
+    @Override
+    public void run() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (more_exhaustion.contains(p)) {
+                for (OriginContainer origin : OriginPlayer.getOrigin(p).values()) {
+                    PowerContainer power = origin.getPowerFileFromType("origins:exhaust");
+                    if (power == null) continue;
+                    if (power.getInterval() == null) {
+                        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Unable to parse interval for origins:exhaust");
+                        return;
+                    }
+                    interval = power.getInterval();
+                    if (ticksE < interval) {
+                        ticksE++;
+                        return;
+                    } else {
+                        if(ConditionExecutor.check(p, origin, "origins:exhaust", null, p)){
+                            p.setExhaustion(p.getExhaustion() - Float.parseFloat(origin.getPowerFileFromType("origins:exhaust").get("exhaustion")));
+                        }
+                        ticksE = 0;
+                    }
+                }
+            }
+        }
+    }
+}
