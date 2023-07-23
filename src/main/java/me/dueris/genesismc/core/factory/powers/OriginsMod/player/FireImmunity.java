a362da14af8d9116f9f39c3503fbc7370d136ca3
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-07-23 16:38:32 -0700

    made fire_immunity use Condition checks

--- src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/player/FireImmunity.java
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/player/FireImmunity.java
@@ -1,5 +1,8 @@
 package me.dueris.genesismc.core.factory.powers.OriginsMod.player;
 
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,10 +17,14 @@ public class FireImmunity implements Listener {
         if (e.getEntity().isDead()) return;
         if (e.getEntity() == null) return;
         if (e.getEntity() instanceof Player p) {
-            if (fire_immunity.contains(p)) {
-                if (e.getCause().equals(EntityDamageEvent.DamageCause.FIRE) || e.getCause().equals(EntityDamageEvent.DamageCause.HOT_FLOOR) || e.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK) || e.getCause().equals(EntityDamageEvent.DamageCause.LAVA)) {
-                    e.setCancelled(true);
-                    e.setDamage(0);
+            for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
+                if (fire_immunity.contains(p)) {
+                    if(ConditionExecutor.check(p, origin, "origins:fire_immunity", null, p)) {
+                        if (e.getCause().equals(EntityDamageEvent.DamageCause.FIRE) || e.getCause().equals(EntityDamageEvent.DamageCause.HOT_FLOOR) || e.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK) || e.getCause().equals(EntityDamageEvent.DamageCause.LAVA)) {
+                            e.setCancelled(true);
+                            e.setDamage(0);
+                        }
+                    }
                 }
             }
         }
