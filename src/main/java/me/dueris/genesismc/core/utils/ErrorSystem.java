966a454de2830c9267357f34a3a77e48852d8c26
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 12:45:11 -0700

    added error system

---
+++ src/main/java/me/dueris/genesismc/core/utils/ErrorSystem.java
@@ -0,0 +1,11 @@
+package me.dueris.genesismc.core.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class ErrorSystem {
+    public void throwError(String error, String power, Player player, OriginContainer origin, LayerContainer layer){
+        Bukkit.getLogger().warning("[Origins-GenesisMC] Generated custom-origin exception at power:[%power%] with error of %error%".replace("%error%", error).replace("%power%", power));
+        Bukkit.getLogger().warning("Player:[%player%], Origin:[%origin%], Layer:[%layer%]".replace("%player%", player.getName()).replace("%origin%", origin.getTag()).replace("%layer%", layer.getTag().toString()));
+    }
+}
