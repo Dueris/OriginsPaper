9562038ae6991b12ce2f9001178df21b4e3bcd47
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 14:14:59 -0700

    added origins:modify_damage_taken

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/value_modifying/ModifyDamageTakenPower.java
@@ -0,0 +1,44 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.value_modifying;
+
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.ErrorSystem;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import java.util.function.BinaryOperator;
+
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.player.attributes.AttributeHandler.getOperationMappingsFloat;
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.value_modifying.ValueModifyingSuperClass.modify_damage_dealt;
+
+public class ModifyDamageTakenPower implements Listener {
+    @EventHandler
+    public void damageEVENT(EntityDamageByEntityEvent e){
+        if(e.getEntity() instanceof Player){
+            Player p = (Player) e.getEntity();
+            if(modify_damage_dealt.contains(p)){
+                for (OriginContainer origin : OriginPlayer.getOrigin(p).values()) {
+                    ValueModifyingSuperClass valueModifyingSuperClass = new ValueModifyingSuperClass();
+                    try {
+                        if (ConditionExecutor.check("bientity_condition", p, origin, "origins:modify_damage_taken", null, e.getDamager())) {
+                            Float value = Float.valueOf(origin.getPowerFileFromType("origins:modify_damage_taken").getModifier().get("value").toString());
+                            String operation = origin.getPowerFileFromType("origins:modify_damage_taken").getModifier().get("operation").toString();
+                            BinaryOperator mathOperator = getOperationMappingsFloat().get(operation);
+                            if (mathOperator != null) {
+                                float result = (float) mathOperator.apply(e.getDamage(), value);
+                                e.setDamage(result);
+                            }
+                        }
+                    } catch (Exception ev) {
+                        ErrorSystem errorSystem = new ErrorSystem();
+                        errorSystem.throwError("unable to get bi-entity", "origins:modify_damage_taken", p, origin, OriginPlayer.getLayer(p, origin));
+                        ev.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
