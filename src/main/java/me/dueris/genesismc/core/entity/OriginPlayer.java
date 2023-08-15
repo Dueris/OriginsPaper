a48c6af77bb3c4f1eba4a41199e67b6af22fbfc1
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 14:39:47 -0700

    added origins:modify_falling cases

--- src/main/java/me/dueris/genesismc/core/entity/OriginPlayer.java
+++ src/main/java/me/dueris/genesismc/core/entity/OriginPlayer.java
@@ -362,6 +362,7 @@ public class OriginPlayer {
                 case "origins:modify_damage_taken" -> modify_damage_taken.add(player);
                 case "origins:modify_damage_dealt" -> modify_damage_dealt.add(player);
                 case "origins:modify_exhaustion" -> modify_exhaustion.add(player);
+                case "origins:modify_falling" -> modify_falling.add(player);
 
                 //actions
                 case "origins:action_on_being_used" -> action_on_being_used.add(player);
@@ -509,6 +510,7 @@ public class OriginPlayer {
                 case "origins:modify_damage_taken" -> modify_damage_taken.remove(player);
                 case "origins:modify_damage_dealt" -> modify_damage_dealt.remove(player);
                 case "origins:modify_exhaustion" -> modify_exhaustion.remove(player);
+                case "origins:modify_falling" -> modify_falling.remove(player);
 
                 //actions
                 case "origins:action_on_being_used" -> action_on_being_used.remove(player);
