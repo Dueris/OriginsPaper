cdc8c82f425da63f7907953b7853fff8e4dc13d4
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 17:17:37 -0700

    added origins:modify_projectile_damage cases

--- src/main/java/me/dueris/genesismc/core/entity/OriginPlayer.java
+++ src/main/java/me/dueris/genesismc/core/entity/OriginPlayer.java
@@ -369,6 +369,7 @@ public class OriginPlayer {
                 case "origins:modify_jump" -> modify_jump.add(player);
                 case "origins:modify_lava_speed" -> modify_lava_speed.add(player);
                 case "origins:modify_player_spawn" -> modify_world_spawn.add(player);
+                case "origins:modify_projectile_damage" -> modify_projectile_damage.add(player);
 
                 //actions
                 case "origins:action_on_being_used" -> action_on_being_used.add(player);
@@ -523,6 +524,7 @@ public class OriginPlayer {
                 case "origins:modify_jump" -> modify_jump.remove(player);
                 case "origins:modify_lava_speed" -> modify_lava_speed.remove(player);
                 case "origins:modify_player_spawn" -> modify_world_spawn.remove(player);
+                case "origins:modify_projectile_damage" -> modify_projectile_damage.remove(player);
 
                 //actions
                 case "origins:action_on_being_used" -> action_on_being_used.remove(player);
