627a9b77d3dcbba93723cab1289e48715b5d747d
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 19:23:41 -0700

    added PreventSleep

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/prevent/PreventSleep.java
@@ -0,0 +1,32 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.prevent;
+
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.prevent.PreventSuperClass.prevent_sleep;
+import static me.dueris.genesismc.core.factory.powers.entity.FreshAir.beds;
+
+public class PreventSleep implements Listener {
+    @EventHandler
+    public void run(PlayerInteractEvent e){
+        if(e.getClickedBlock() == null) return;
+        if(beds.contains(e.getClickedBlock().getType())){
+            if(!prevent_sleep.contains(e.getPlayer())) return;
+            for(OriginContainer origin : OriginPlayer.getOrigin(e.getPlayer()).values()){
+                if(ConditionExecutor.check("block_condition", e.getPlayer(), origin, "origins:prevent_sleep", null, e.getPlayer())){
+                    if(origin.getPowerFileFromType("origins:prevent_sleep").get("set_spawn_point", "false").toString() == "true"){
+                        e.getPlayer().setBedSpawnLocation(e.getClickedBlock().getLocation());
+                    }
+                    if(origin.getPowerFileFromType("origins:prevent_sleep").get("message", "origins.cant_sleep") != null){
+                        e.getPlayer().sendMessage(origin.getPowerFileFromType("origins:prevent_sleep").get("message", "origins.cant_sleep"));
+                    }
+                    e.setCancelled(true);
+                }
+            }
+        }
+    }
+}
