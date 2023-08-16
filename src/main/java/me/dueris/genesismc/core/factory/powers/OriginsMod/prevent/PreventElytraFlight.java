75ae2e68ad4ec93bbf85af5646a7bbc3a764558f
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 19:23:02 -0700

    added PreventElytraFlight

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/prevent/PreventElytraFlight.java
@@ -0,0 +1,27 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.prevent;
+
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.prevent.PreventSuperClass.prevent_elytra_flight;
+
+public class PreventElytraFlight implements Listener {
+    @EventHandler
+    public void run(EntityToggleGlideEvent e){
+        if(e.getEntity() instanceof Player p){
+            if(prevent_elytra_flight.contains(p)){
+                for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
+                    if(ConditionExecutor.check("condition", p, origin, "origins:prevent_elytra_flight", null, p)){
+                        e.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+}
