f5c553b01a399b1c0eda1e3272676f64dc832b90
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 19:23:48 -0700

    added PreventSprinting

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/prevent/PreventSprinting.java
@@ -0,0 +1,29 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.prevent;
+
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.prevent.PreventSuperClass.prevent_sprinting;
+
+public class PreventSprinting extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        for(Player p : Bukkit.getOnlinePlayers()){
+            if(prevent_sprinting.contains(p)){
+                for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
+                    if(ConditionExecutor.check("condition", p, origin, "origins:prevent_sprinting", null, p)){
+                        p.setSprinting(false);
+                    }
+                }
+            }
+        }
+    }
+}
