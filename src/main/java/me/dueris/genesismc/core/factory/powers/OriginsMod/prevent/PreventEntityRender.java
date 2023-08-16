26b6de986beba53f454e5f14b0dcf56335dbf50e
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-15 19:23:17 -0700

    added PreventEntityRender

---
+++ src/main/java/me/dueris/genesismc/core/factory/powers/OriginsMod/prevent/PreventEntityRender.java
@@ -0,0 +1,36 @@
+package me.dueris.genesismc.core.factory.powers.OriginsMod.prevent;
+
+import me.dueris.genesismc.core.GenesisMC;
+import me.dueris.genesismc.core.entity.OriginPlayer;
+import me.dueris.genesismc.core.factory.conditions.ConditionExecutor;
+import me.dueris.genesismc.core.utils.OriginContainer;
+import net.minecraft.network.PacketListener;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static me.dueris.genesismc.core.factory.powers.OriginsMod.prevent.PreventSuperClass.prevent_entity_render;
+
+public class PreventEntityRender extends BukkitRunnable {
+    @Override
+    public void run() {
+        for(Player p : Bukkit.getOnlinePlayers()){
+            if(prevent_entity_render.contains(p)){
+                for(OriginContainer origin : OriginPlayer.getOrigin(p).values()){
+                    for(Entity entity : p.getWorld().getEntities()){
+                        if(ConditionExecutor.check("entity_condition", p, origin, "origins:prevent_entity_render", null, p)){
+                            if(ConditionExecutor.check("bientity_condition", p, origin, "origins:prevent_entity_render", null, p)){
+                                p.hideEntity(GenesisMC.getPlugin(), entity);
+                            }else{
+                                p.showEntity(GenesisMC.getPlugin(), entity);
+                            }
+                        }else{
+                            p.showEntity(GenesisMC.getPlugin(), entity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
