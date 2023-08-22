cfea012c09cebfada80428bd2454cb45bfdcbab5
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-08-22 12:07:38 -0700

    patched conditions

--- src/main/java/me/dueris/genesismc/core/factory/conditions/ConditionExecutor.java
+++ src/main/java/me/dueris/genesismc/core/factory/conditions/ConditionExecutor.java
@@ -8,18 +8,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class ConditionExecutor {
-    public static boolean check(String singular, String plural, Player p, OriginContainer origin, String powerfile, EntityDamageEvent dmgevent, Entity entity) {
-        if(origin.getPowerFileFromType(powerfile) == null) return false;
-        if (origin.getPowerFileFromType(powerfile).getConditionFromString(singular, plural) == null)
+    public boolean check(String singular, String plural, Player p, OriginContainer origin, String powerfile, EntityDamageEvent dmgevent, Entity entity) {
+        if(origin == null) return true;
+        if(origin.getPowerFileFromType(powerfile) == null) return true;
+        if (origin.getPowerFileFromType(powerfile).getConditionFromString(singular, plural) == null){
             return true;
-
+        }
         if (dmgevent != null) {
-            if (DamageCondition.check(singular, plural, p, origin, powerfile, dmgevent) == "true")
+            if (DamageCondition.check(singular, plural, p, origin, powerfile, dmgevent) == "true"){
                 return true;
+            }
         }
         if (entity != null) {
-            if (EntityCondition.check(singular, plural, p, origin, powerfile, entity) == "true")
+            if (EntityCondition.check(singular, plural, p, origin, powerfile, entity) == "true"){
                 return true;
+            }
         }
 
         //final check
