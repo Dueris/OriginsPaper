250992ec3965cfd7ee8560419ca4e57c83a98e9f
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-09-01 13:10:17 -0700

    added origins:ingredient

--- src/main/java/me/dueris/genesismc/factory/conditions/item/ItemCondition.java
+++ src/main/java/me/dueris/genesismc/factory/conditions/item/ItemCondition.java
@@ -1,9 +1,13 @@
 package me.dueris.genesismc.factory.conditions.item;
 
+import me.dueris.genesismc.utils.OriginContainer;
+import me.dueris.genesismc.utils.PowerContainer;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class ItemCondition {
@@ -13,7 +17,22 @@ public class ItemCondition {
         String type = condition.get("type").toString().toLowerCase();
 
         switch (type) {
-
+            case "origins:ingredient" -> {
+                p.sendMessage("ingredient");
+                if (condition.containsKey("ingredient")) {
+                    Map<String, Object> ingredientMap = (Map<String, Object>) condition.get("ingredient");
+                    if (ingredientMap.containsKey("item")) {
+                        String itemValue = ingredientMap.get("item").toString();
+                        p.sendMessage(itemValue);
+                        p.sendMessage(String.valueOf(Material.valueOf(itemValue.toString().split(":")[1].toUpperCase())));
+                        p.sendMessage(item.getType().toString());
+                        if(item.getType().equals(Material.valueOf(itemValue.toString().split(":")[1].toUpperCase()))){
+                            p.sendMessage("test");
+                            return Optional.of(true);
+                        }
+                    }
+                }
+            }
         }
         return Optional.of(false);
     }
