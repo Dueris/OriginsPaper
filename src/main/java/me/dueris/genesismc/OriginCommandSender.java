b2c2b6710b09297a3154d0b0f7f0b12659d88c1a
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-09-09 15:25:30 -0700

    created new command sender

---
+++ src/main/java/me/dueris/genesismc/OriginCommandSender.java
@@ -0,0 +1,216 @@
+package me.dueris.genesismc;
+
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.audience.MessageType;
+import net.kyori.adventure.chat.ChatType;
+import net.kyori.adventure.chat.SignedMessage;
+import net.kyori.adventure.identity.Identified;
+import net.kyori.adventure.identity.Identity;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.util.TriState;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class OriginCommandSender implements CommandSender {
+    @Override
+    public void sendMessage(@NotNull String message) {
+
+    }
+
+    @Override
+    public void sendMessage(@NotNull String... messages) {
+
+    }
+
+    @Override
+    public void sendMessage(@Nullable UUID sender, @NotNull String message) {
+
+    }
+
+    @Override
+    public void sendMessage(@Nullable UUID sender, @NotNull String... messages) {
+
+    }
+
+    @Override
+    public @NotNull Server getServer() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return null;
+    }
+
+    @Override
+    public @NotNull Spigot spigot() {
+        return null;
+    }
+
+    @Override
+    public @NotNull Component name() {
+        return null;
+    }
+
+    @Override
+    public void sendRichMessage(@NotNull String message) {
+        CommandSender.super.sendRichMessage(message);
+    }
+
+    @Override
+    public void sendPlainMessage(@NotNull String message) {
+        CommandSender.super.sendPlainMessage(message);
+    }
+
+    @Override
+    public boolean isPermissionSet(@NotNull String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isPermissionSet(@NotNull Permission perm) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(@NotNull String name) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(@NotNull Permission perm) {
+        return false;
+    }
+
+    @Override
+    public @NotNull PermissionAttachment addAttachment(@NotNull Plugin plugin, @NotNull String name, boolean value) {
+        return null;
+    }
+
+    @Override
+    public @NotNull PermissionAttachment addAttachment(@NotNull Plugin plugin) {
+        return null;
+    }
+
+    @Override
+    public @Nullable PermissionAttachment addAttachment(@NotNull Plugin plugin, @NotNull String name, boolean value, int ticks) {
+        return null;
+    }
+
+    @Override
+    public @Nullable PermissionAttachment addAttachment(@NotNull Plugin plugin, int ticks) {
+        return null;
+    }
+
+    @Override
+    public void removeAttachment(@NotNull PermissionAttachment attachment) {
+
+    }
+
+    @Override
+    public void recalculatePermissions() {
+
+    }
+
+    @Override
+    public @NotNull Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        return null;
+    }
+
+    @Override
+    public @NotNull TriState permissionValue(@NotNull Permission permission) {
+        return CommandSender.super.permissionValue(permission);
+    }
+
+    @Override
+    public @NotNull TriState permissionValue(@NotNull String permission) {
+        return CommandSender.super.permissionValue(permission);
+    }
+
+    @Override
+    public boolean isOp() {
+        return false;
+    }
+
+    @Override
+    public void setOp(boolean value) {
+
+    }
+
+    @Override
+    public @NotNull Audience filterAudience(@NotNull Predicate<? super Audience> filter) {
+        return CommandSender.super.filterAudience(filter);
+    }
+
+    @Override
+    public void forEachAudience(@NotNull Consumer<? super Audience> action) {
+        CommandSender.super.forEachAudience(action);
+    }
+
+    @Override
+    public void sendMessage(@NotNull ComponentLike message) {
+        CommandSender.super.sendMessage(message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Component message) {
+        CommandSender.super.sendMessage(message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Identified source, @NotNull ComponentLike message) {
+        CommandSender.super.sendMessage(source, message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Identity source, @NotNull ComponentLike message) {
+        CommandSender.super.sendMessage(source, message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Identified source, @NotNull Component message) {
+        CommandSender.super.sendMessage(source, message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Identity source, @NotNull Component message) {
+        CommandSender.super.sendMessage(source, message);
+    }
+
+    @Override
+    public void sendMessage(@NotNull Component message, ChatType.@NotNull Bound boundChatType) {
+        CommandSender.super.sendMessage(message, boundChatType);
+    }
+
+    @Override
+    public void sendMessage(@NotNull ComponentLike message, ChatType.@NotNull Bound boundChatType) {
+        CommandSender.super.sendMessage(message, boundChatType);
+    }
+
+    @Override
+    public void sendMessage(@NotNull SignedMessage signedMessage, ChatType.@NotNull Bound boundChatType) {
+        CommandSender.super.sendMessage(signedMessage, boundChatType);
+    }
+
+    @Override
+    public void deleteMessage(@NotNull SignedMessage signedMessage) {
+        CommandSender.super.deleteMessage(signedMessage);
+    }
+
+    @Override
+    public void deleteMessage(SignedMessage.@NotNull Signature signature) {
+        CommandSender.super.deleteMessage(signature);
+    }
+}
