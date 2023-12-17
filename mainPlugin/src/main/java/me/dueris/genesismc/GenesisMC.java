8cf32372ad14171cedb70393e577146b3efdc081
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-12-17 12:28:41 -0800

    i added placeholder api hook

--- mainPlugin/src/main/java/me/dueris/genesismc/GenesisMC.java
+++ mainPlugin/src/main/java/me/dueris/genesismc/GenesisMC.java
@@ -39,6 +39,7 @@ import me.dueris.genesismc.factory.powers.simple.MimicWarden;
 import me.dueris.genesismc.files.GenesisDataFiles;
 import me.dueris.genesismc.files.TempStorageContainer;
 import me.dueris.genesismc.generation.WaterProtBookGen;
+import me.dueris.genesismc.hooks.papi.PlaceholderApiExtension;
 import me.dueris.genesismc.items.GenesisItems;
 import me.dueris.genesismc.items.InfinPearl;
 import me.dueris.genesismc.items.OrbOfOrigins;
@@ -87,6 +88,7 @@ public final class GenesisMC extends JavaPlugin implements Listener {
     private static GenesisMC plugin;
     public static ConditionExecutor conditionExecutor;
     public static String apoliVersion = "1.11.3";
+    public static boolean placeholderapi = false;
 
     static {
         tool = EnumSet.of(Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SHOVEL, Material.DIAMOND_SWORD, Material.GOLDEN_AXE, Material.GOLDEN_HOE, Material.GOLDEN_PICKAXE, Material.GOLDEN_SHOVEL, Material.GOLDEN_SWORD, Material.NETHERITE_AXE, Material.NETHERITE_HOE, Material.NETHERITE_PICKAXE, Material.NETHERITE_SHOVEL, Material.NETHERITE_SWORD, Material.IRON_AXE, Material.IRON_HOE, Material.IRON_PICKAXE, Material.IRON_SHOVEL, Material.IRON_SWORD, Material.WOODEN_AXE, Material.WOODEN_HOE, Material.WOODEN_PICKAXE, Material.WOODEN_SHOVEL, Material.WOODEN_SWORD, Material.SHEARS);
@@ -178,6 +180,10 @@ public final class GenesisMC extends JavaPlugin implements Listener {
         if(LangConfig.getLangFile() == null){
             Bukkit.getLogger().severe("Unable to start GenesisMC due to lang not being loaded properly");
             Bukkit.getServer().getPluginManager().disablePlugin(this);
+        }
+        placeholderapi = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
+        if(placeholderapi){
+            new PlaceholderApiExtension(this).register();
         }
 
         // load tempStorageOptimizations - start
---
+++ mainPlugin/src/main/java/me/dueris/genesismc/hooks/papi/PlaceholderApiExtension.java
@@ -0,0 +1,85 @@
+package me.dueris.genesismc.hooks.papi;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.dueris.genesismc.GenesisMC;
+import me.dueris.genesismc.OriginDataContainer;
+import me.dueris.genesismc.entity.OriginPlayer;
+import me.dueris.genesismc.factory.CraftApoli;
+import me.dueris.genesismc.utils.OriginContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PlaceholderApiExtension extends PlaceholderExpansion {
+    private GenesisMC plugin;
+
+    public PlaceholderApiExtension(GenesisMC plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "origins";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "dueris";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return GenesisMC.pluginVersion;
+    }
+
+    @Override
+    public @Nullable String getRequiredPlugin() {
+        return "GenesisMC";
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    @Override
+    public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+        return onPlaceholderRequest(player.getPlayer(), params);
+    }
+
+    @Override
+    public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
+        if(params.equalsIgnoreCase("player_origin")){
+            String done = "";
+            for(OriginContainer origin : OriginPlayer.getOrigin(player).values()){
+                done = done + origin.getTag() + "//";
+            }
+            return done;
+        }
+        if(params.equalsIgnoreCase("player_layer")){
+            String done = "";
+            for(OriginContainer origin : OriginPlayer.getOrigin(player).values()){
+                done = done + origin.getLayerTag() + "//";
+            }
+            return done;
+        }
+        if(params.equalsIgnoreCase("player_origin_data")){
+            return OriginDataContainer.getLayer(player);
+        }
+        if(params.equalsIgnoreCase("all_origins")){
+            return CraftApoli.getOrigins().toString();
+        }
+        if(params.equalsIgnoreCase("all_layers")){
+            return CraftApoli.getLayers().toString();
+        }
+
+        return "wtf"; // Unkown placeholder
+    }
+}
