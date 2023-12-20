e1db178339583ad13be61dc6449291a240e01c9e
Dueris MC <purplewolf.mc1@gmail.com>
2023-12-20 18:02:58 +0000

    gonna add this along with the ticking to fix some issues

--- mainPlugin/src/main/java/me/dueris/genesismc/factory/powers/player/RestrictArmor.java
+++ mainPlugin/src/main/java/me/dueris/genesismc/factory/powers/player/RestrictArmor.java
@@ -11,8 +11,12 @@ import me.dueris.genesismc.utils.translation.LangConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,7 +26,7 @@ import java.util.Optional;
 import static me.dueris.genesismc.factory.conditions.ConditionExecutor.getResult;
 import static me.dueris.genesismc.utils.ArmorUtils.getArmorValue;
 
-public class RestrictArmor extends CraftPower {
+public class RestrictArmor extends CraftPower implements Listener{
 
     private Long interval;
     private final int ticksE;
@@ -53,6 +57,22 @@ public class RestrictArmor extends CraftPower {
         }
     }
 
+    @EventHandler
+    public void tick(PlayerArmorChangeEvent e){
+        Player p = e.getPlayer();
+        if (getPowerArray().contains(p)) {
+            for (OriginContainer origin : OriginPlayer.getOrigin(p).values()) {
+                for (PowerContainer power : origin.getMultiPowerFileFromType(getPowerFile())) {
+                    if (power == null) continue;
+                    ConditionExecutor executor = me.dueris.genesismc.GenesisMC.getConditionExecutor();
+                        if (executor.check("condition", "conditions", p, power, getPowerFile(), p, null, p.getLocation().getBlock(), null, p.getItemInHand(), null)) {
+                            runPower(p, power);
+                        }
+                }
+            }
+        }
+    }
+
     @Override
     public void setActive(Player p, String tag, Boolean bool) {
         if(powers_active.containsKey(p)){
@@ -69,7 +89,6 @@ public class RestrictArmor extends CraftPower {
 
     public void run(Player p, HashMap<Player, Integer> ticksEMap) {
         ticksEMap.putIfAbsent(p, 0);
-
         if (getPowerArray().contains(p)) {
             for (OriginContainer origin : OriginPlayer.getOrigin(p).values()) {
                 for (PowerContainer power : origin.getMultiPowerFileFromType(getPowerFile())) {
@@ -87,144 +106,148 @@ public class RestrictArmor extends CraftPower {
                     } else {
                         ConditionExecutor executor = me.dueris.genesismc.GenesisMC.getConditionExecutor();
                         if (executor.check("condition", "conditions", p, power, getPowerFile(), p, null, p.getLocation().getBlock(), null, p.getItemInHand(), null)) {
-                            setActive(p, power.getTag(), true);
-                            boolean headb = true;
-                            boolean chestb = true;
-                            boolean legsb = true;
-                            boolean feetb = true;
-
-                            if (power.getHead() == null) headb = false;
-                            if (power.getChest() == null) chestb = false;
-                            if (power.getLegs() == null) legsb = false;
-                            if (power.getFeet() == null) feetb = false;
-
-                            if (power.getHead().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
-                                String comparisonh = power.getHead().get("comparison").toString();
-                                String comparisontoh = power.getHead().get("compare_to").toString();
-                                if (!headb) return;
-                                ItemStack item = p.getInventory().getHelmet();
-                                if (item != null) {
-                                    double armorValue = getArmorValue(item);
-                                    double compareValue = Double.parseDouble(comparisontoh);
-                                    if (compareValues(armorValue, comparisonh, compareValue)) {
-                                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.HEAD);
-                                    }
-                                }
-                            } else if (power.getHead().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
-                                if (!headb) return;
-                                if(p.getInventory().getHelmet() != null){
-                                    Map<String, Object> ingredientMap = (Map<String, Object>) power.getHead().get("ingredient");
-                                    if (ingredientMap.containsKey("item")) {
-                                        String itemValue = ingredientMap.get("item").toString();
-                                        String item = null;
-                                        if(itemValue.contains(":")){
-                                            item = itemValue.split(":")[1];
-                                        }else{
-                                            item = itemValue;
-                                        }
-                                        if(p.getInventory().getHelmet().getType().equals(Material.valueOf(item.toUpperCase()))) {
-                                            OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.HEAD);
-                                        }
-                                    }
-                                }
-                            }
-
-                            if (power.getChest().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
-                                String comparisonc = power.getChest().get("comparison").toString();
-                                String comparisontoc = power.getChest().get("compare_to").toString();
-                                if (!chestb) return;
-                                ItemStack item = p.getInventory().getChestplate();
-                                if (item != null) {
-                                    double armorValue = getArmorValue(item);
-                                    double compareValue = Double.parseDouble(comparisontoc);
-                                    if (compareValues(armorValue, comparisonc, compareValue)) {
-                                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.CHEST);
-                                    }
-                                }
-                            } else if (power.getChest().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
-                                if (!chestb) return;
-                                if(p.getInventory().getChestplate() != null){
-                                    Map<String, Object> ingredientMap = (Map<String, Object>) power.getChest().get("ingredient");
-                                    if (ingredientMap.containsKey("item")) {
-                                        String itemValue = ingredientMap.get("item").toString();
-                                        String item = null;
-                                        if(itemValue.contains(":")){
-                                            item = itemValue.split(":")[1];
-                                        }else{
-                                            item = itemValue;
-                                        }
-                                        if(p.getInventory().getChestplate().getType().equals(Material.valueOf(item.toUpperCase()))) {
-                                            OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.CHEST);
-                                        }
-                                    }
-                                }
-                            }
-
-                            if (power.getLegs().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
-                                String comparisonl = power.getLegs().get("comparison").toString();
-                                String comparisontol = power.getLegs().get("compare_to").toString();
-                                if (!legsb) return;
-                                ItemStack item = p.getInventory().getLeggings();
-                                if (item != null) {
-                                    double armorValue = getArmorValue(item);
-                                    double compareValue = Double.parseDouble(comparisontol);
-                                    if (compareValues(armorValue, comparisonl, compareValue)) {
-                                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.LEGS);
-                                    }
-                                }
-                            } else if (power.getLegs().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
-                                if (!legsb) return;
-                                if(p.getInventory().getLeggings() != null){
-                                    Map<String, Object> ingredientMap = (Map<String, Object>) power.getLegs().get("ingredient");
-                                    if (ingredientMap.containsKey("item")) {
-                                        String itemValue = ingredientMap.get("item").toString();
-                                        String item = null;
-                                        if(itemValue.contains(":")){
-                                            item = itemValue.split(":")[1];
-                                        }else{
-                                            item = itemValue;
-                                        }
-                                        if(p.getInventory().getLeggings().getType().equals(Material.valueOf(item.toUpperCase()))) {
-                                            OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.LEGS);
-                                        }
-                                    }
-                                }
-                            }
-
-                            if (power.getFeet().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
-                                String comparisonf = power.getFeet().get("comparison").toString();
-                                String comparisontof = power.getFeet().get("compare_to").toString();
-                                if (!feetb) return;
-                                ItemStack item = p.getInventory().getBoots();
-                                if (item != null) {
-                                    double armorValue = getArmorValue(item);
-                                    double compareValue = Double.parseDouble(comparisontof);
-                                    if (compareValues(armorValue, comparisonf, compareValue)) {
-                                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.FEET);
-                                    }
-                                }
-                            } else if (power.getFeet().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
-                                if (!feetb) return;
-                                if(p.getInventory().getBoots() != null){
-                                    Map<String, Object> ingredientMap = (Map<String, Object>) power.getFeet().get("ingredient");
-                                    if (ingredientMap.containsKey("item")) {
-                                        String itemValue = ingredientMap.get("item").toString();
-                                        String item = null;
-                                        if(itemValue.contains(":")){
-                                            item = itemValue.split(":")[1];
-                                        }else{
-                                            item = itemValue;
-                                        }
-                                        if(p.getInventory().getBoots().getType().equals(Material.valueOf(item.toUpperCase()))) {
-                                            OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.FEET);
-                                        }
-                                    }
-                                }
-                            }
+                            runPower(p, power);
                         } else {
                             setActive(p, power.getTag(), false);
                         }
                         ticksEMap.put(p, 0);
+                    }
+                }
+            }
+        }
+    }
+
+    public void runPower(Player p, PowerContainer power){
+        setActive(p, power.getTag(), true);
+        boolean headb = true;
+        boolean chestb = true;
+        boolean legsb = true;
+        boolean feetb = true;
+
+        if (power.getHead() == null) headb = false;
+        if (power.getChest() == null) chestb = false;
+        if (power.getLegs() == null) legsb = false;
+        if (power.getFeet() == null) feetb = false;
+
+        if (power.getHead().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
+            String comparisonh = power.getHead().get("comparison").toString();
+            String comparisontoh = power.getHead().get("compare_to").toString();
+            if (!headb) return;
+            ItemStack item = p.getInventory().getHelmet();
+            if (item != null) {
+                double armorValue = getArmorValue(item);
+                double compareValue = Double.parseDouble(comparisontoh);
+                if (compareValues(armorValue, comparisonh, compareValue)) {
+                    OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.HEAD);
+                }
+            }
+        } else if (power.getHead().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
+            if (!headb) return;
+            if(p.getInventory().getHelmet() != null){
+                Map<String, Object> ingredientMap = (Map<String, Object>) power.getHead().get("ingredient");
+                if (ingredientMap.containsKey("item")) {
+                    String itemValue = ingredientMap.get("item").toString();
+                    String item = null;
+                    if(itemValue.contains(":")){
+                        item = itemValue.split(":")[1];
+                    }else{
+                        item = itemValue;
+                    }
+                    if(p.getInventory().getHelmet().getType().equals(Material.valueOf(item.toUpperCase()))) {
+                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.HEAD);
+                    }
+                }
+            }
+        }
+
+        if (power.getChest().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
+            String comparisonc = power.getChest().get("comparison").toString();
+            String comparisontoc = power.getChest().get("compare_to").toString();
+            if (!chestb) return;
+            ItemStack item = p.getInventory().getChestplate();
+            if (item != null) {
+                double armorValue = getArmorValue(item);
+                double compareValue = Double.parseDouble(comparisontoc);
+                if (compareValues(armorValue, comparisonc, compareValue)) {
+                    OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.CHEST);
+                }
+            }
+        } else if (power.getChest().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
+            if (!chestb) return;
+            if(p.getInventory().getChestplate() != null){
+                Map<String, Object> ingredientMap = (Map<String, Object>) power.getChest().get("ingredient");
+                if (ingredientMap.containsKey("item")) {
+                    String itemValue = ingredientMap.get("item").toString();
+                    String item = null;
+                    if(itemValue.contains(":")){
+                        item = itemValue.split(":")[1];
+                    }else{
+                        item = itemValue;
+                    }
+                    if(p.getInventory().getChestplate().getType().equals(Material.valueOf(item.toUpperCase()))) {
+                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.CHEST);
+                    }
+                }
+            }
+        }
+
+        if (power.getLegs().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
+            String comparisonl = power.getLegs().get("comparison").toString();
+            String comparisontol = power.getLegs().get("compare_to").toString();
+            if (!legsb) return;
+            ItemStack item = p.getInventory().getLeggings();
+            if (item != null) {
+                double armorValue = getArmorValue(item);
+                double compareValue = Double.parseDouble(comparisontol);
+                if (compareValues(armorValue, comparisonl, compareValue)) {
+                    OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.LEGS);
+                }
+            }
+        } else if (power.getLegs().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
+            if (!legsb) return;
+            if(p.getInventory().getLeggings() != null){
+                Map<String, Object> ingredientMap = (Map<String, Object>) power.getLegs().get("ingredient");
+                if (ingredientMap.containsKey("item")) {
+                    String itemValue = ingredientMap.get("item").toString();
+                    String item = null;
+                    if(itemValue.contains(":")){
+                        item = itemValue.split(":")[1];
+                    }else{
+                        item = itemValue;
+                    }
+                    if(p.getInventory().getLeggings().getType().equals(Material.valueOf(item.toUpperCase()))) {
+                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.LEGS);
+                    }
+                }
+            }
+        }
+
+        if (power.getFeet().get("type").toString().equalsIgnoreCase("origins:armor_value")) {
+            String comparisonf = power.getFeet().get("comparison").toString();
+            String comparisontof = power.getFeet().get("compare_to").toString();
+            if (!feetb) return;
+            ItemStack item = p.getInventory().getBoots();
+            if (item != null) {
+                double armorValue = getArmorValue(item);
+                double compareValue = Double.parseDouble(comparisontof);
+                if (compareValues(armorValue, comparisonf, compareValue)) {
+                    OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.FEET);
+                }
+            }
+        } else if (power.getFeet().get("type").toString().equalsIgnoreCase("origins:ingredient")) {
+            if (!feetb) return;
+            if(p.getInventory().getBoots() != null){
+                Map<String, Object> ingredientMap = (Map<String, Object>) power.getFeet().get("ingredient");
+                if (ingredientMap.containsKey("item")) {
+                    String itemValue = ingredientMap.get("item").toString();
+                    String item = null;
+                    if(itemValue.contains(":")){
+                        item = itemValue.split(":")[1];
+                    }else{
+                        item = itemValue;
+                    }
+                    if(p.getInventory().getBoots().getType().equals(Material.valueOf(item.toUpperCase()))) {
+                        OriginPlayer.moveEquipmentInventory(p, EquipmentSlot.FEET);
                     }
                 }
             }
