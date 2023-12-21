b0c6f5d56e902c9a126dbe917d7f989d664cd949
Dueris MC <purplewolf.mc1@gmail.com>
2023-12-21 02:24:53 +0000

    include a tiny bit more registry hooks

--- mainPlugin/src/main/java/me/dueris/genesismc/utils/Utils.java
+++ mainPlugin/src/main/java/me/dueris/genesismc/utils/Utils.java
@@ -1,6 +1,5 @@
 package me.dueris.genesismc.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_20_R3.CraftRegistry;
 import org.bukkit.craftbukkit.v1_20_R3.CraftServer;
 
@@ -11,14 +10,16 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.biome.Biome;
 
 public class Utils {
     public MinecraftServer server = MinecraftServer.getServer();
     public CraftServer bukkitServer = server.server;
     public static Registry<DamageType> DAMAGE_REGISTRY = CraftRegistry.getMinecraftRegistry().registryOrThrow(Registries.DAMAGE_TYPE);
+    public static Registry<Biome> BIOME_REGISTRY = CraftRegistry.getMinecraftRegistry().registryOrThrow(Registries.BIOME);
+    public static Registry<Enchantment> ENCHANTMENT_REGISTRY = CraftRegistry.getMinecraftRegistry().registryOrThrow(Registries.ENCHANTMENT);
 
     public static DamageSource getDamageSource(DamageType type){
         DamageSource source = null;
@@ -29,6 +30,10 @@ public class Utils {
             }
         }
         return source;
+    }
+
+    public static Registry<?> getRegistry(ResourceKey<Registry<?>> registry){
+        return CraftRegistry.getMinecraftRegistry().registryOrThrow(registry);
     }
 
     public static Enchantment registerEnchantment(String name, Enchantment enchantment) {
