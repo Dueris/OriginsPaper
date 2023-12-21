d3b9944419ce74adaa276df682ca95f59d6d0f67
Dueris MC <purplewolf.mc1@gmail.com>
2023-12-21 01:35:08 +0000

    added other methods to reloading

--- mainPlugin/src/main/java/me/dueris/genesismc/commands/subcommands/origin/Reload.java
+++ mainPlugin/src/main/java/me/dueris/genesismc/commands/subcommands/origin/Reload.java
@@ -1,10 +1,22 @@
 package me.dueris.genesismc.commands.subcommands.origin;
 
+import me.dueris.genesismc.GenesisMC;
 import me.dueris.genesismc.OriginDataContainer;
 import me.dueris.genesismc.commands.subcommands.SubCommand;
 import me.dueris.genesismc.factory.CraftApoli;
+import me.dueris.genesismc.factory.TagRegistry;
+import me.dueris.genesismc.factory.powers.CraftPower;
+import me.dueris.genesismc.utils.OriginContainer;
+import me.dueris.genesismc.utils.PowerContainer;
+
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
+import com.mojang.logging.LogUtils;
 
 public class Reload extends SubCommand {
     @Override
@@ -28,8 +40,27 @@ public class Reload extends SubCommand {
         sender.sendMessage(ChatColor.RED + "Use this command with caution. This could result in data loss, errors, and possibly a crash of the server.");
         try {
             CraftApoli.unloadData();
-            CraftApoli.loadOrigins();
+            CraftPower.getRegistered().clear();
             OriginDataContainer.unloadAllData();
+            CraftApoli.loadOrigins();
+            try {
+                for (Class<? extends CraftPower> c : CraftPower.findCraftPowerClasses()) {
+                    if (CraftPower.class.isAssignableFrom(c)) {
+                        CraftPower instance = c.newInstance();
+                        CraftPower.getRegistered().add(instance.getClass());
+                        if (instance instanceof Listener || Listener.class.isAssignableFrom(instance.getClass())) {
+                            Bukkit.getServer().getPluginManager().registerEvents((Listener) instance, GenesisMC.getPlugin());
+                        }
+                    }
+                }
+                for (OriginContainer origin : CraftApoli.getOrigins()) {
+                    for (PowerContainer powerContainer : origin.getPowerContainers()) {
+                        CraftApoli.getPowers().add(powerContainer);
+                    }
+                }
+            } catch (IOException | ReflectiveOperationException e) {
+                e.printStackTrace();
+            }
             OriginDataContainer.loadData();
             sender.sendMessage(ChatColor.GREEN + "Origins reloaded successfully.");
         } catch (Exception e) {
