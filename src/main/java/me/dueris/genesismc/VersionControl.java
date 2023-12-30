b540ea5ccb27628fb5a745eea1effb0f1cb7186f
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-12-30 15:27:57 -0800

    version bump

--- src/main/java/me/dueris/genesismc/VersionControl.java
+++ src/main/java/me/dueris/genesismc/VersionControl.java
@@ -20,7 +20,7 @@ public class VersionControl {
     }
 
     public static void pluginVersionCheck() {
-        String pluginVersion = "0.2.5";
+        String pluginVersion = "0.2.6";
 
         try {
             URL url = new URL("https://raw.githubusercontent.com/Dueris/GenesisMC/origin/version.txt");
