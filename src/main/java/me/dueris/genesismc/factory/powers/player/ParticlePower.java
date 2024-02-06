b2410a35135de4e10954c210563034abdf58bf73
PurpleWolfMC <jedimastertoothless@hotmail.com>
2024-02-05 18:53:24 -0800

    Fix some things with particle power

--- src/main/java/me/dueris/genesismc/factory/powers/player/ParticlePower.java
+++ src/main/java/me/dueris/genesismc/factory/powers/player/ParticlePower.java
@@ -4,9 +4,11 @@ import me.dueris.genesismc.entity.OriginPlayerUtils;
 import me.dueris.genesismc.factory.conditions.ConditionExecutor;
 import me.dueris.genesismc.factory.powers.CraftPower;
 import me.dueris.genesismc.utils.PowerContainer;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,30 +42,32 @@ public class ParticlePower extends CraftPower {
                         if (executor.check("condition", "conditions", player, power, getPowerFile(), player, null, null, null, player.getInventory().getItemInHand(), null)) {
                             if (!getPowerArray().contains(player)) return;
                             setActive(player, power.getTag(), true);
-                            Particle particle = Particle.valueOf(power.getStringOrDefault("particle", null).split(":")[1].toUpperCase());
+                            Particle particle = computeParticleArgs(power.getObject("particle"));
+                            if(particle == null) throw new IllegalStateException("Unable to create CraftBukkit particle instance");
+                            if(particle.equals(Particle.DUST_COLOR_TRANSITION)) throw new IllegalStateException("DustColorTransitions are currently not supported in this version");
                             int count = power.getIntOrDefault("count", 1);
                             float offset_y_no_vector = power.getFloatOrDefault("offset_y", 1.0f);
                             float offset_x = 0.25f;
                             float offset_y = 0.50f;
                             float offset_z = 0.25f;
+
                             if (power.get("spread").get("y") != null) {
                                 offset_y = Float.parseFloat(String.valueOf(power.get("spread").get("y")));
                             }
-
                             if (power.get("spread").get("x") != null) {
                                 offset_x = Float.parseFloat(String.valueOf(power.get("spread").get("x")));
                             }
-
                             if (power.get("spread").get("z") != null) {
                                 offset_z = Float.parseFloat(String.valueOf(power.get("spread").get("z")));
                             }
+
                             boolean visible_while_invis = power.getBooleanOrDefault("visible_while_invisible", false);
                             Particle final_particle = particle.builder().count(count).force(true).location(player.getLocation()).particle(particle).source(player).offset(offset_x, offset_y + offset_y_no_vector, offset_z).particle();
                             if (visible_while_invis) {
-                                player.getWorld().spawnParticle(particle, new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()), count, offset_x, offset_y, offset_z, 0);
+                                player.getWorld().spawnParticle(final_particle, new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()), count, offset_x, offset_y, offset_z, 0);
                             } else {
                                 if (!player.isInvisible()) {
-                                    player.getWorld().spawnParticle(particle, new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()), count, offset_x, offset_y, offset_z, 0);
+                                    player.getWorld().spawnParticle(final_particle, new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()), count, offset_x, offset_y, offset_z, 0);
                                 }
                             }
                         } else {
@@ -74,6 +78,21 @@ public class ParticlePower extends CraftPower {
                 }
             }
         }
+    }
+
+    public static Particle computeParticleArgs(Object root){
+        if(root instanceof String particleSt){
+            return Particle.valueOf(ensureCorrectNamespace(particleSt).split(":")[1].toUpperCase());
+        } else if (root instanceof JSONObject particle) {
+            System.out.println(ensureCorrectNamespace(particle.get("type").toString()));
+            return Particle.valueOf(ensureCorrectNamespace(particle.get("type").toString()).split(":")[1].toUpperCase());
+        }
+        return null;
+    }
+
+    private static String ensureCorrectNamespace(String string){
+        System.out.println(string.contains(":"));
+        return string.contains(":") ? string : "minecraft:" + string;
     }
 
     @Override
