2ae033c4ec0d3d1f2d4d4bb7b25aebfab1f70c02
PurpleWolfMC <jedimastertoothless@hotmail.com>
2024-02-09 18:05:19 -0800

    Add DynamicNamespace support

---
+++ src/main/java/me/dueris/genesismc/utils/text/NamespaceUtils.java
@@ -0,0 +1,24 @@
+package me.dueris.genesismc.utils.text;
+
+import org.bukkit.NamespacedKey;
+
+public class NamespaceUtils {
+    public static NamespacedKey getDynamicNamespace(String currentNamespace, String dynamicNamespace){
+        if(dynamicNamespace.contains("*")){
+            String[] split = dynamicNamespace.split(":");
+            String namespace = split[0];
+            String path = split[1];
+            if(path.contains("*")){
+                path.replace("*", NamespacedKey.fromString(currentNamespace).asString().split(":")[1]);
+            }
+            if(namespace.contains("*")){
+                namespace.replace("*", NamespacedKey.fromString(currentNamespace).asString().split(":")[0]);
+            }
+        }
+        return NamespacedKey.fromString(dynamicNamespace);
+    }
+
+    public static NamespacedKey getNamespaceStringAsBukkit(String key){
+        return NamespacedKey.fromString(key);
+    }
+}
