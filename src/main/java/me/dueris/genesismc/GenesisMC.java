6e610a88621afdefb8bdbd8a20d3df146241a081
PurpleWolfMC <jedimastertoothless@hotmail.com>
2024-02-11 14:32:24 -0800

    Bump version

--- src/main/java/me/dueris/genesismc/GenesisMC.java
+++ src/main/java/me/dueris/genesismc/GenesisMC.java
@@ -99,7 +99,7 @@ public final class GenesisMC extends JavaPlugin implements Listener {
     public static OriginScheduler.OriginSchedulerTree scheduler = null;
     public static String version = Bukkit.getVersion().split("\\(MC: ")[1].replace(")", "");
     public static boolean isCompatible = false;
-    public static String pluginVersion = "v0.2.6";
+    public static String pluginVersion = "v0.2.7";
     public static String world_container = MinecraftServer.getServer().options.asMap().toString().split(", \\[W, universe, world-container, world-dir]=\\[")[1].split("], ")[0];
     public static ExecutorService loaderThreadPool;
     public static ArrayList<String> versions = new ArrayList<>();
--- src/main/java/me/dueris/genesismc/VersionControl.java
+++ src/main/java/me/dueris/genesismc/VersionControl.java
@@ -17,7 +17,7 @@ public class VersionControl {
     }
 
     public static void pluginVersionCheck() {
-        String pluginVersion = "0.2.6";
+        String pluginVersion = "0.2.7";
 
         try {
             URL url = new URL("https://raw.githubusercontent.com/Dueris/GenesisMC/origin/version.txt");
