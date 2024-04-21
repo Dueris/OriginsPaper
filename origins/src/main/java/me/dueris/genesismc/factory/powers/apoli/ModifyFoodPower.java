782d2199a21587cdae224ed0f97d1853d00c3ed3
PurpleWolfMC <jedimastertoothless@hotmail.com>
2024-04-20 18:39:54 -0700

    Finish apoli:modify_food

--- origins/src/main/java/me/dueris/genesismc/factory/powers/apoli/ModifyFoodPower.java
+++ origins/src/main/java/me/dueris/genesismc/factory/powers/apoli/ModifyFoodPower.java
@@ -2,7 +2,9 @@ package me.dueris.genesismc.factory.powers.apoli;
 
 import me.dueris.calio.builder.inst.factory.FactoryElement;
 import me.dueris.calio.builder.inst.factory.FactoryJsonObject;
+import me.dueris.genesismc.GenesisMC;
 import me.dueris.genesismc.factory.CraftApoli;
+import me.dueris.genesismc.factory.actions.Actions;
 import me.dueris.genesismc.factory.conditions.ConditionExecutor;
 import me.dueris.genesismc.factory.powers.CraftPower;
 import me.dueris.genesismc.registry.registries.Layer;
@@ -13,8 +15,11 @@ import org.bukkit.craftbukkit.v1_20_R3.entity.CraftEntity;
 import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.function.BinaryOperator;
@@ -22,15 +27,16 @@ import java.util.function.BinaryOperator;
 import static me.dueris.genesismc.factory.powers.apoli.superclass.ValueModifyingSuperClass.modify_food;
 
 public class ModifyFoodPower extends CraftPower implements Listener {
+    public static ArrayList<Player> preventEffects = new ArrayList<>();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void saturationorwhateverRUN(PlayerItemConsumeEvent e) {
         Player player = e.getPlayer();
         for (Layer layer : CraftApoli.getLayersFromRegistry()) {
             if (modify_food.contains(player)) {
                 for (Power power : OriginPlayerAccessor.getMultiPowerFileFromType(player, getPowerFile(), layer)) {
                     if (ConditionExecutor.testEntity(power.getJsonObject("condition"), (CraftEntity) player) && ConditionExecutor.testItem(power.getJsonObject("item_condition"), e.getItem())) {
-                        if (modify_food.contains(player)) {
+                        if (power.isPresent("food_modifier")) {
                             for (FactoryJsonObject jsonObject : power.getList$SingularPlural("food_modifier", "food_modifiers").stream().map(FactoryElement::toJsonObject).toList()) {
                                 if (jsonObject.isPresent("value")) {
                                     int val = jsonObject.getNumber("value").getInt();
@@ -44,6 +50,8 @@ public class ModifyFoodPower extends CraftPower implements Listener {
                                     }
                                 }
                             }
+                        }
+                        if (power.isPresent("saturation_modifier")) {
                             for (FactoryJsonObject jsonObject : power.getList$SingularPlural("saturation_modifier", "saturation_modifiers").stream().map(FactoryElement::toJsonObject).toList()) {
                                 if (jsonObject.isPresent("value")) {
                                     int val = jsonObject.getNumber("value").getInt();
@@ -57,11 +65,35 @@ public class ModifyFoodPower extends CraftPower implements Listener {
                                 }
                             }
                         }
+
+                        if (power.isPresent("replace_stack")) {
+                            e.setReplacement(power.getItemStack("result_stack"));
+                        }
+                        Actions.executeEntity(player, power.getJsonObject("entity_action"));
+                        Actions.executeItem(e.getItem(), power.getJsonObject("item_action"));
+
+                        if (power.getBooleanOrDefault("prevent_effects", false) && !e.isCancelled()) {
+                            preventEffects.add(player);
+                            new BukkitRunnable() {
+                                @Override
+                                public void run() {
+                                    preventEffects.remove(player);
+                                }
+                            }.runTaskLater(GenesisMC.getPlugin(), 1);
+                        }
                     } else {
                         setActive(player, power.getTag(), false);
                     }
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void effectAddEvent(EntityPotionEffectEvent e) {
+        if (e.getEntity() instanceof Player p && e.getCause().equals(EntityPotionEffectEvent.Cause.FOOD) && preventEffects.contains(p)) {
+            e.setCancelled(true);
+            e.setOverride(false);
         }
     }
 
