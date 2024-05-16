a6a27ae237d0cc670dd25f2c40299c97737b8507
PurpleWolfMC <jedimastertoothless@hotmail.com>
2024-05-15 17:29:46 -0700

    Parity

--- origins/src/main/java/me/dueris/genesismc/factory/powers/apoli/ActionOnDeath.java
+++ origins/src/main/java/me/dueris/genesismc/factory/powers/apoli/ActionOnDeath.java
@@ -16,32 +16,27 @@ public class ActionOnDeath extends PowerType {
 	private final FactoryJsonObject bientityAction;
 	private final FactoryJsonObject bientityCondition;
 	private final FactoryJsonObject damageCondition;
-	private final FactoryJsonObject entityAction;
 
-	public ActionOnDeath(String name, String description, boolean hidden, FactoryJsonObject condition, int loading_priority, FactoryJsonObject bientityAction, FactoryJsonObject bientityCondition, FactoryJsonObject damageCondition, FactoryJsonObject entityAction) {
+	public ActionOnDeath(String name, String description, boolean hidden, FactoryJsonObject condition, int loading_priority, FactoryJsonObject bientityAction, FactoryJsonObject bientityCondition, FactoryJsonObject damageCondition) {
 		super(name, description, hidden, condition, loading_priority);
 		this.bientityAction = bientityAction;
 		this.bientityCondition = bientityCondition;
 		this.damageCondition = damageCondition;
-		this.entityAction = entityAction;
 	}
 
 	public static FactoryData registerComponents(FactoryData data) {
 		return PowerType.registerComponents(data).ofNamespace(GenesisMC.apoliIdentifier("action_on_death"))
 			.add("bientity_action", FactoryJsonObject.class, new FactoryJsonObject(new JsonObject()))
 			.add("bientity_condition", FactoryJsonObject.class, new FactoryJsonObject(new JsonObject()))
-			.add("damage_condition", FactoryJsonObject.class, new FactoryJsonObject(new JsonObject()))
-			.add("entity_action", FactoryJsonObject.class, new FactoryJsonObject(new JsonObject()));
+			.add("damage_condition", FactoryJsonObject.class, new FactoryJsonObject(new JsonObject()));
 	}
 
 	@EventHandler
 	public void d(EntityDeathEvent e) {
 		if (e.getEntity() instanceof Player p) {
 			if (getPlayers().contains(p)) {
-				if (!isActive(p) || !ConditionExecutor.testDamage(damageCondition, e.getEntity().getLastDamageCause()))
-					return;
-				Actions.executeEntity(p, entityAction);
-				if (((CraftPlayer) p).getHandle().getLastHurtByMob() != null && ConditionExecutor.testBiEntity(bientityCondition, ((CraftPlayer) p).getHandle().getLastHurtByMob().getBukkitEntity(), p)) {
+				if (!isActive(p) || !ConditionExecutor.testDamage(damageCondition, e.getEntity().getLastDamageCause())) return;
+				if (ConditionExecutor.testBiEntity(bientityCondition, ((CraftPlayer) p).getHandle().getLastHurtByMob().getBukkitEntity(), p)) {
 					Actions.executeBiEntity(((CraftPlayer) p).getHandle().getLastHurtByMob().getBukkitEntity(), p/* player is target? */, bientityAction);
 				}
 			}
